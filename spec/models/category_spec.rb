dbd7350658684207a193b79a18163145d1b4f1fe
puppe1990 <matheus.puppe90@hotmail.com>
2021-05-25 16:02:31 -0300

    Install factory and create more categories tests

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,16 +3,21 @@ require 'rails_helper'
 RSpec.describe Category, type: :model do
 
   it 'save the correct data' do
-    account = Account.create(company_name: 'Empresa teste')
-
-    category = Category.new(name: 'Vestido', account_id: account.id)
-
+    user = create(:user)
+    account = Account.create(company_name: FFaker::InternetSE.company_name_single_word, user_id: user.id)
+    category = Category.new(name: FFaker::Internet.slug, account_id: account.id)
     expect(category).to be_valid
   end
 
   it 'save without account' do
-    category = Category.new(name: 'Vestido')
+    category = Category.new(name: FFaker::Internet.slug)
+    expect(category).to_not be_valid
+  end
 
+  it 'category is not valid without name' do
+    user = create(:user)
+    account = Account.create(company_name: FFaker::InternetSE.company_name_single_word, user_id: user.id)
+    category = Category.new(name: nil, account_id: account.id)
     expect(category).to_not be_valid
   end
 end
