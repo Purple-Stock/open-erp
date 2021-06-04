6ab02fb11fde901927b4c71e736a04456c51af0b
puppe1990 <matheus.puppe90@hotmail.com>
2021-06-04 15:52:05 -0300

    Using factories in category spec

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,9 +3,7 @@ require 'rails_helper'
 RSpec.describe Category, type: :model do
 
   it 'save the correct data' do
-    user = create(:user)
-    account = Account.create(company_name: FFaker::InternetSE.company_name_single_word, user_id: user.id)
-    category = Category.new(name: FFaker::Internet.slug, account_id: account.id)
+    category = create(:category)
     expect(category).to be_valid
   end
 
@@ -15,8 +13,7 @@ RSpec.describe Category, type: :model do
   end
 
   it 'category is not valid without name' do
-    user = create(:user)
-    account = Account.create(company_name: FFaker::InternetSE.company_name_single_word, user_id: user.id)
+    account = create(:account)
     category = Category.new(name: nil, account_id: account.id)
     expect(category).to_not be_valid
   end
