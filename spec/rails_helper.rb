462d5962250cc91eb2731ed7c0bdebbb3c6b04cb
Jorge Borges <jorgetrabalho2016@gmail.com>
2021-10-27 00:04:22 -0300

    Aumenta cobertura de testes

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -37,7 +37,9 @@ end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
+  config.include Warden::Test::Helpers
+  include LoginUser
+  
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
---
+++ spec/support/request_helper.rb
@@ -0,0 +1,12 @@
+module RequestHelper
+  def body_json(symbolize_key: false)
+    json = JSON.parse(response.body)
+    symbolize_key ? json.symbolize_keys : json
+  rescue 
+    return {}
+  end
+end
+
+RSpec.configure do |config|
+  config.include RequestHelper, type: :request
+end
