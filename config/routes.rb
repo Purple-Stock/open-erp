b1204c9b9d5859d379e0391f8e0560cdcde54a96
puppe1990 <matheus.puppe90@hotmail.com>
2021-10-27 15:07:09 -0300

    new api route to save sale products

--- config/routes.rb
+++ config/routes.rb
@@ -44,6 +44,7 @@ Rails.application.routes.draw do
       get 'sale_products/:id', to: 'products#show_product', as: 'show_product'
       get 'products', to: 'products#index', as: 'index'
       post 'purchase_products/add_products', to: 'purchase_products#add_products', as: 'add_products'
+      post 'sale_products/remove_products', to: 'sale_products#remove_products', as: 'remove_products'
       post 'purchase_products/add_inventory_quantity', to: 'purchase_products#add_inventory_quantity',
                                                        as: 'add_inventory_quantity'
     end
