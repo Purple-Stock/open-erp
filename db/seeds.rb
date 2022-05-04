5ea100a29ced56dfbc6ff9e3794fe6a573a5a873
JonatascNascimento <jonatascn@gmail.com>
2022-05-04 00:04:49 -0300

    Use Faker to generate user email on seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -8,4 +9,4 @@
 
 p "Module User"
 
-FactoryBot.create(:user, password: '123456', email: 'fashion.store@email.com')
+FactoryBot.create(:user, password: '123456', email: Faker::Internet.email)
