a8af1935a807df4e7edd88fa038dfb8a554c0832
JonatascNascimento <jonatascn@gmail.com>
2022-05-13 00:42:19 -0300

    Removing bin/setup from main yaml and faker from seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,3 @@
-require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -9,4 +8,4 @@ require 'faker'
 
 p "Module User"
 
-FactoryBot.create(:user, password: '123456', email: Faker::Internet.email)
+FactoryBot.create(:user, password: '123456', email: 'fashion.store@email.com')
