7cd6567b110bffe690fe32ea0087d4aba0346f9b
JonatascNascimento <jonatascn@gmail.com>
2022-05-13 00:26:28 -0300

    Changing simplo products id to uuid

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_13_031844) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_13_032413) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -243,7 +243,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_13_031844) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "simplo_products", force: :cascade do |t|
+  create_table "simplo_products", id: :uuid, default: nil, force: :cascade do |t|
+    t.bigint "integer_id", default: -> { "nextval('simplo_products_id_seq'::regclass)" }, null: false
     t.string "name"
     t.string "sku"
     t.datetime "created_at", null: false
