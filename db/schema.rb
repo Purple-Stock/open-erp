c7562b8ac8ad2f23b0406baaad171023786c4750
JonatascNascimento <jonatascn@gmail.com>
2022-05-13 00:23:46 -0300

    Changing simplo order id to uuid

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_13_031438) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_13_031844) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -210,10 +210,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_13_031438) do
     t.bigint "integer_id", default: -> { "nextval('simplo_items_id_seq'::regclass)" }, null: false
     t.string "sku"
     t.integer "quantity"
-    t.integer "simplo_order_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.uuid "product_id"
+    t.uuid "simplo_order_id"
     t.index ["product_id"], name: "index_simplo_items_on_product_id"
     t.index ["simplo_order_id"], name: "index_simplo_items_on_simplo_order_id"
   end
@@ -233,7 +233,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_13_031438) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "simplo_orders", force: :cascade do |t|
+  create_table "simplo_orders", id: :uuid, default: nil, force: :cascade do |t|
+    t.bigint "integer_id", default: -> { "nextval('simplo_orders_id_seq'::regclass)" }, null: false
     t.string "client_name"
     t.string "order_id"
     t.string "order_status"
@@ -287,5 +288,4 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_13_031438) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "simplo_items", "simplo_orders"
 end
