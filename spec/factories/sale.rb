804b45cbafb9d2be73315a0d9e6529354d5afcba
puppe1990 <matheus.puppe90@hotmail.com>
2022-10-17 13:06:28 -0300

    create factory and test for sales

---
+++ spec/factories/sale.rb
@@ -0,0 +1,14 @@
+require_relative '../support/factory_helpers'
+
+FactoryBot.define do
+  factory :sale do
+    value { rand(100..400) }
+    online { [true, false].sample }
+    customer_id { create(:customer).id }
+    payment_type { rand(0..6) }
+    #store_sale
+    total_exchange_value { rand(100..400) }
+
+    account_id { create(:account).id }
+  end
+end
