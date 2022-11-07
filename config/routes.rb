e7129d4f19e4a82f37e71e0495b116b6b8636f92
puppe1990 <matheus.puppe90@hotmail.com>
2022-11-07 15:57:41 -0300

    remove unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -28,8 +28,6 @@ Rails.application.routes.draw do
   resources :categories
   resources :groups
   resources :group_products
-  get 'companies/new'
-  post 'companies/create'
   get 'group_lists', to: 'groups#show_group_product', as: 'show_group_product'
   get 'confection_lists', to: 'groups#show_product_confection', as: 'show_product_confection'
   get 'orders_control', to: 'orders_control#show_orders_control', as: 'show_orders_control'
