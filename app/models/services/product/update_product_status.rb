242ca734a35deb0f6c02b1a73c61e25be5918163
puppe1990 <matheus.puppe90@hotmail.com>
2022-11-08 10:58:13 -0300

    extract update product active to service

---
+++ app/models/services/product/update_product_status.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Services
+  module Product
+    class UpdateProductStatus < ApplicationService
+      attr_reader :product
+
+      def initialize(product)
+        @product = product
+      end
+
+      def call    
+        if @product.active.eql? true
+          @product.update(active: false)
+        else
+          @product.update(active: true)
+        end
+      rescue StandardError
+        errors.add(:active, message: 'não pode ser atualizado')
+      end
+    end
+  end
+end
