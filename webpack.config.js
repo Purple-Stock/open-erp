65092fbc1cf632c8f53c6570b07798614965ee10
puppe1990 <matheus.puppe90@hotmail.com>
2022-12-11 09:01:52 -0300

    change config webpacker

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require("webpack")
 
 module.exports = {
   mode: "production",
-  devtool: "source-map",
+  devtool: "false",
   entry: {
     application: "./app/javascript/application.js"
   },
