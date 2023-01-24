38005f6dbad39efec7c88e941167ef7f66a4ac7b
jhonnatas <jhonnatas.aires@gmail.com>
2023-01-23 23:40:05 -0300

    feature/add language switch button to navbar

---
+++ app/controllers/locales_controller.rb
@@ -0,0 +1,9 @@
+class LocalesController < ApplicationController
+  
+  before_action :set_locale
+
+  def set_locale
+    session[:locale] = params[:locale] if params[:locale].present?
+    I18n.locale = session[:locale] || I18n.default_locale
+  end 
+end
