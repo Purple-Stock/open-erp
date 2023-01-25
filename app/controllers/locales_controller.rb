0df663e39df364499d58cd887aa71e90bd66d74d
jhonnatas <jhonnatas.aires@gmail.com>
2023-01-25 20:02:00 -0300

    feature/add locales controller

--- app/controllers/locales_controller.rb
+++ app/controllers/locales_controller.rb
@@ -5,5 +5,11 @@ class LocalesController < ApplicationController
   def set_locale
     session[:locale] = params[:locale] if params[:locale].present?
     I18n.locale = session[:locale] || I18n.default_locale
+
+    redirect_to request.referrer
   end 
+
 end
+
+
+
