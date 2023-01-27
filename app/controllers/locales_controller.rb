e51a0344a643f77746a90cecf3597a2b7b5e93cb
jhonnatas <jhonnatas.aires@gmail.com>
2023-01-27 16:32:22 -0300

    feature/read store localize from cookie and apply changes to app

--- app/controllers/locales_controller.rb
+++ app/controllers/locales_controller.rb
@@ -12,20 +12,12 @@ class LocalesController < ApplicationController
       cookies.permanent[:locale] = params[:locale] # save cookies
     end
 
-    #if params[:locale].present?
-      #locale = params[:locale] || I18n.default_locale
-      #cookies.permanent[:locale] = params[:locale] # save cookies
-    #end    
-   
     locale = cookies[:locale]&.to_sym # this read cookies
   
     if I18n.available_locales.include?(locale)
       I18n.locale = locale # use cookies locale
       redirect_to request.referrer # to the same page
-    else
-      I18n.default_locale
-      redirect_to request.referrer
-    end
+    end    
   end
 
 end
