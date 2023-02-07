76268f30e731bc35563346e6c44cb1a0a1697208
jhonnatas <jhonnatas.aires@gmail.com>
2023-02-07 16:29:18 -0300

    feature/add default language in switch language

--- app/controllers/locales_controller.rb
+++ app/controllers/locales_controller.rb
@@ -2,19 +2,21 @@ class LocalesController < ApplicationController
   
   before_action :set_locale
 
-  def default_url_option
-    cookie.delete(:locale)
-    locale =  I18n.locale
-    redirect_to request.referrer    
+  def default_locale_option
+    cookies.delete(:locale)   
+    cookies.permanent[:locale] = I18n.default_locale # save cookies with default language 
   end
 
   def set_locale
     if params[:locale].present?
-      locale = params[:locale] 
-      cookies.permanent[:locale] = params[:locale] # save cookies
+      if params[:locale] == 'default'
+        default_locale_option
+      else
+        cookies.permanent[:locale] = params[:locale] # save cookies    
+      end
     end
 
-    locale = cookies[:locale]&.to_sym # this read cookies
+    locale = cookies[:locale]&.to_sym # this reads cookies
   
     if I18n.available_locales.include?(locale)
       I18n.locale = locale # use cookies locale
