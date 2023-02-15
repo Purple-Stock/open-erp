89ce82cd0191b2063670a47e7e07c643c3644cd9
puppe1990 <matheus.puppe90@hotmail.com>
2023-02-15 11:28:30 -0300

    remove commented code and improve readbility

--- app/javascript/sale.js
+++ app/javascript/sale.js
@@ -5,14 +5,8 @@ $(document).on('turbo:load').ready( function () {
         setTimeout(function(){$('.form-control-sm').select2(); }, 1);
     });
 
-
     onScan.attachTo(document, {
         suffixKeyCodes: [13], // enter-key expected at the end of a scan
-        // onKeyProcess: function(sChar, oEvent){
-        //     console.log(sChar);
-        //     console.log(oEvent);
-        //     console.log(oEvent.keyCode);
-        // },
         keyCodeMapper: function(oEvent) {
             // Look for special keycodes or other event properties specific to
             // your scanner
@@ -38,7 +32,6 @@ $(document).on('turbo:load').ready( function () {
             }
         },
         onScan: function(sCode) { // Alternative to document.addEventListener('scan')
-            // console.log(sCode);
             fetchProducts(JSON.parse(sCode));
         }
     });
@@ -46,15 +39,15 @@ $(document).on('turbo:load').ready( function () {
     var product_data;
 
     function fetchProducts(custom_qr) {
-        fetch('/api/v1/products/' + custom_qr.custom_id).then(function (res) {
-            if (res.ok) {
-                return res.json();
+        fetch('/api/v1/products/' + custom_qr.custom_id).then(function (response) {
+            if (response.ok) {
+                return response.json();
             }
         }).then(function (data) {
             product_data = data
             document.querySelector(".links > a").click();
-        }).catch(function (err) {
-            return console.log(err);
+        }).catch(function (error) {
+            return console.log(error);
         });
     }
 
@@ -63,40 +56,8 @@ $(document).on('turbo:load').ready( function () {
         insertedItem[0].querySelectorAll('input')[0].value = 1;
         insertedItem[0].querySelectorAll('input')[1].value = product_data.price;
     });
-
-    // $('#sales-ajax').dataTable({
-    //     ajax: '/sales_defer',
-    //     deferRender: true,
-    //     dataSrc: function (json) {
-    //         json.recordsTotal = json.meta.recordsTotal;
-    //         json.recordsFiltered = json.meta.recordsFiltered;
-    //     },
-    //     columns: [
-    //         { title: "Cliente", searchable: true, data: 'attributes.name' },
-    //         { title: "Desconto em R$", searchable: false, data: 'attributes.discount', orderable: false },
-    //         { title: "Online", data: 'attributes.online', searchable: false },
-    //         { title: "Código Pedido Online", searchable: true, data: 'attributes.order_code' },
-    //         { title: "Valor", data: 'attributes.value', searchable: false,  },
-    //         { title: "Divulgação", data: 'attributes.disclosure', searchable: false, orderable: false },
-    //         { title: "Troca", data: 'attributes.exchange', searchable: false, orderable: false },
-    //         { title: "Data de criação", data: 'attributes.created_at', searchable: true },
-    //         { title: "Ações", data: 'attributes.id', render: function(id){
-    //             return '<a class="btn btn-default btn-xs" href="/sales/'+ id + '">Ver</a><a class="btn btn-default btn-xs" href="/sales/'+ id + '/edit">Editar</a><a data-confirm="Você tem certeza?" data-method="delete" rel="nofollow" class="btn btn-danger btn-xs" href="/sales/'+ id + '">Deletar</a>';
-    //         }}
-    //     ],
-    //     serverSide: true,
-    //     "language": {
-    //         "url": "https://cdn.datatables.net/plug-ins/1.10.12/i18n/Portuguese-Brasil.json"
-    //     },
-    //     columnDefs: [
-    //         { type: 'formatted-num', targets: 0 }
-    //     ],
-    //     "order": [[ 3, "desc" ]],
-    //     responsive: true,
-    //     stateSave: true
-    // });
-
 });
+
 function fetchSaleProducts(element) {
     const row = element.parentElement.parentElement.parentElement;
     const quantity = row.querySelector('.quantity').value;
@@ -107,7 +68,7 @@ function fetchSaleProducts(element) {
         }
     }).then(function (data) {
         row.querySelector('.product-value').value = (data.price * parseInt(quantity)).toFixed(2);
-    }).catch(function (err) {
-        return console.log(err);
+    }).catch(function (error) {
+        return console.log(error);
     });
 }
