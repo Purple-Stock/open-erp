155d2b51dad612c809c8dc9e9fa469733d67414b
puppe1990 <matheus.puppe90@hotmail.com>
2023-08-06 12:03:32 -0300

    update code

--- app/controllers/api/v1/bling/orders_controller.rb
+++ app/controllers/api/v1/bling/orders_controller.rb
@@ -24,17 +24,32 @@ module Api
                                        'Accept' => '1.0',
                                        'Authorization' => "Basic #{Base64.strict_encode64("#{params[:code]}")}"
                                      })
-            BlingDatum.create(access_token: response[:access_token],
-                             expires_in: response[:expires_in],
-                             expires_at: Time.zone.now + response[:expires_in].seconds,
-                             token_type: response[:token_type],
-                             scope: response[:scope],
-                             refresh_token: response[:refresh_token],
-                             account_id: current_tenant.id)
-
+            verify_tokens
             render json: response.parsed_response
           rescue StandardError => e
             render json: { error: e.message }, status: :internal_server_error
+          end
+        end
+
+        private
+
+        def verify_tokens
+          tokens = BlingDatum.find(account_id: current_tenant.id)
+          if tokens.nil?
+            BlingDatum.create(access_token: response[:access_token],
+                              expires_in: response[:expires_in],
+                              expires_at: Time.zone.now + response[:expires_in].seconds,
+                              token_type: response[:token_type],
+                              scope: response[:scope],
+                              refresh_token: response[:refresh_token],
+                              account_id: current_tenant.id)
+          else
+            tokens.update(access_token: response[:access_token],
+                          expires_in: response[:expires_in],
+                          expires_at: Time.zone.now + response[:expires_in].seconds,
+                          token_type: response[:token_type],
+                          scope: response[:scope],
+                          refresh_token: response[:refresh_token])
           end
         end
       end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,23 +1,23 @@
 class HomeController < ApplicationController
   def index
-    begin 
-      response = Services::Bling::Order.call(order_command: 'find_orders', tenant: current_user.account.id)
+    response = Services::Bling::Order.call(order_command: 'find_orders', tenant: current_user.account.id)
 
-      @orders = response['data']
+    @orders = response['data']
 
-      order_ids = @orders.select { |order| order['loja']['id'] == 204_061_683 }.map { |order| order['id'] }
+    order_ids = @orders.select { |order| order['loja']['id'] == 204_061_683 }.map { |order| order['id'] }
 
-      @mercado_envios_flex_counts = count_mercado_envios_flex(order_ids)
+    @mercado_envios_flex_counts = count_mercado_envios_flex(order_ids)
 
-      @store_name = get_loja_name
+    @store_name = get_loja_name
 
-      @loja_ids = [204_219_105, 203_737_982, 203_467_890, 204_061_683]
+    @loja_ids = [204_219_105, 203_737_982, 203_467_890, 204_061_683]
 
-      @last_update = format_last_update(Time.current)
-    rescue StandardError => e
-      Rails.logger.error(e.message)
-      redirect_to home_last_updates_path
-    end
+    @expires_at = format_last_update(token_expires_at)
+
+    @last_update = format_last_update(Time.current)
+  rescue StandardError => e
+    Rails.logger.error(e.message)
+    redirect_to home_last_updates_path
   end
 
   def last_updates
@@ -30,7 +30,8 @@ class HomeController < ApplicationController
   def count_mercado_envios_flex(order_ids)
     counter = 0
     order_ids.each do |order_id|
-      response = Services::Bling::FindOrder.call(id: order_id, order_command: 'find_order', tenant: current_user.account.id)
+      response = Services::Bling::FindOrder.call(id: order_id, order_command: 'find_order',
+                                                 tenant: current_user.account.id)
       order = response['data']
 
       shipping = order['transporte']
@@ -42,6 +43,10 @@ class HomeController < ApplicationController
 
   def format_last_update(time)
     time.strftime('%d-%m-%Y %H:%M:%S')
+  end
+
+  def token_expires_at
+    BlingDatum.find_by(account_id: current_tenant.id).expires_at
   end
 
   def get_loja_name
