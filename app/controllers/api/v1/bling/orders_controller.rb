1fee9e4e1e23eaf5b972107d18fe0ec4448f400f
puppe1990 <matheus.puppe90@hotmail.com>
2023-08-06 22:43:29 -0300

    save the token

--- app/controllers/api/v1/bling/orders_controller.rb
+++ app/controllers/api/v1/bling/orders_controller.rb
@@ -10,22 +10,25 @@ module Api
 
         def get_token
           code = params[:code]
+          client_id = ENV['CLIENT_ID']
+          client_secret = ENV['CLIENT_SECRET']
+          credentials = Base64.strict_encode64("#{client_id}:#{client_secret}")
 
           return render json: { error: 'Invalid code' }, status: :bad_request if code.blank?
 
           begin
-            response = HTTParty.post('https://bling.com.br/Api/v3/oauth/token',
-                                     body: {
-                                       grant_type: 'authorization_code',
-                                       code:
-                                     },
-                                     headers: {
-                                       'Content-Type' => 'application/x-www-form-urlencoded',
-                                       'Accept' => '1.0',
-                                       'Authorization' => "Basic #{Base64.strict_encode64("#{params[:code]}")}"
-                                     })
+            @response = HTTParty.post('https://bling.com.br/Api/v3/oauth/token',
+                                      body: {
+                                        grant_type: 'authorization_code',
+                                        code:
+                                      },
+                                      headers: {
+                                        'Content-Type' => 'application/x-www-form-urlencoded',
+                                        'Accept' => '1.0',
+                                        'Authorization' => "Basic #{credentials}"
+                                      })
             verify_tokens
-            render json: response.parsed_response
+            render json: @response.parsed_response
           rescue StandardError => e
             render json: { error: e.message }, status: :internal_server_error
           end
@@ -34,22 +37,22 @@ module Api
         private
 
         def verify_tokens
-          tokens = BlingDatum.find(account_id: current_tenant.id)
+          tokens = BlingDatum.find_by(account_id: current_tenant.id)
           if tokens.nil?
-            BlingDatum.create(access_token: response[:access_token],
-                              expires_in: response[:expires_in],
-                              expires_at: Time.zone.now + response[:expires_in].seconds,
-                              token_type: response[:token_type],
-                              scope: response[:scope],
-                              refresh_token: response[:refresh_token],
+            BlingDatum.create(access_token: @response['access_token'],
+                              expires_in: @response['expires_in'],
+                              expires_at: Time.zone.now + @response['expires_in'].seconds,
+                              token_type: @response['token_type'],
+                              scope: @response['scope'],
+                              refresh_token: @response['refresh_token'],
                               account_id: current_tenant.id)
           else
-            tokens.update(access_token: response[:access_token],
-                          expires_in: response[:expires_in],
-                          expires_at: Time.zone.now + response[:expires_in].seconds,
-                          token_type: response[:token_type],
-                          scope: response[:scope],
-                          refresh_token: response[:refresh_token])
+            tokens.update(access_token: @response['access_token'],
+                          expires_in: @response['expires_in'],
+                          expires_at: Time.zone.now + @response['expires_in'].seconds,
+                          token_type: @response['token_type'],
+                          scope: @response['scope'],
+                          refresh_token: @response['refresh_token'])
           end
         end
       end
