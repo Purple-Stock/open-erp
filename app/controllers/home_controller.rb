24652f79b09981682bbda5fc3760edb835e67de2
puppe1990 <matheus.puppe90@hotmail.com>
2023-08-15 23:44:40 -0300

    put the missing product in dash

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,19 +1,24 @@
 class HomeController < ApplicationController
   def index
-    in_progress = Services::Bling::Order.call(order_command: 'find_orders', tenant: current_user.account.id, situation: 15)
+    in_progress = Services::Bling::Order.call(order_command: 'find_orders', tenant: current_user.account.id,
+                                              situation: 15)
 
     @orders = in_progress['data']
 
     checkeds = Services::Bling::Order.call(order_command: 'find_orders', tenant: current_user.account.id, situation: 24)
 
     @checked_orders = checkeds['data']
-    
-    pendings = Services::Bling::Order.call(order_command: 'find_orders', tenant: current_user.account.id, situation: 94871)
-    
+
+    pendings = Services::Bling::Order.call(order_command: 'find_orders', tenant: current_user.account.id,
+                                           situation: 94_871)
+                                           
     @pending_orders = pendings['data']
 
-    printed = Services::Bling::Order.call(order_command: 'find_orders', tenant: current_user.account.id, situation: 95745)
-    
+    update_orders_data
+                                           
+    printed = Services::Bling::Order.call(order_command: 'find_orders', tenant: current_user.account.id,
+                                          situation: 95_745)
+
     @printed_orders = printed['data']
 
     order_ids = @orders.select { |order| order['loja']['id'] == 204_061_683 }.map { |order| order['id'] }
@@ -54,6 +59,46 @@ class HomeController < ApplicationController
       Rails.logger.error(e.message)
     end
     counter
+  end
+
+  def update_orders_data
+    @pending_orders.each do |order_data|
+      order_id = order_data['id']
+
+      if BlingOrderItem.exists?(bling_order_id: order_id)
+        bling_order_items = BlingOrderItem.where(bling_order_id: order_id)
+        if order_data['situacao']['id'] != bling_order_items[0].situation_id
+          bling_order_items.each do |bling_order_item|
+            bling_order_item.update(situation_id: order_data['situacao']['id'])
+          end
+        end
+        next # Skip to the next order
+      end
+
+      fetched_order_data = fetch_order_data(order_id)
+
+      fetched_order_data['data']['itens'].each do |item_data|
+        BlingOrderItem.create!(
+          bling_order_id: order_id,
+          codigo: item_data['codigo'],
+          unidade: item_data['unidade'],
+          quantidade: item_data['quantidade'],
+          desconto: item_data['desconto'],
+          valor: item_data['valor'],
+          aliquotaIPI: item_data['aliquotaIPI'],
+          descricao: item_data['descricao'],
+          descricaoDetalhada: item_data['descricaoDetalhada'],
+          situation_id: fetched_order_data['data']['situacao']['id'],
+          store_id: fetched_order_data['data']['loja']['id']
+        )
+      end
+    end
+  end
+  
+
+  def fetch_order_data(order_id)
+    Services::Bling::FindOrder.call(id: order_id, order_command: 'find_order',
+      tenant: current_user.account.id)
   end
 
   def format_last_update(time)
