49d59ac9f4cb3886ab3e9645f0defefbfb0ca7a9
puppe1990 <matheus.puppe90@hotmail.com>
2023-08-15 23:44:40 -0300

    put the missing product in dash

---
+++ spec/factories/bling_order_items.rb
@@ -0,0 +1,31 @@
+# == Schema Information
+#
+# Table name: bling_order_items
+#
+#  id                 :bigint           not null, primary key
+#  aliquotaIPI        :decimal(, )
+#  codigo             :string
+#  desconto           :decimal(, )
+#  descricao          :text
+#  descricaoDetalhada :text
+#  quantidade         :integer
+#  unidade            :string
+#  valor              :decimal(, )
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  bling_order_id     :string
+#  situation_id       :string
+#  store_id           :string
+#
+FactoryBot.define do
+  factory :bling_order_item do
+    codigo { "MyString" }
+    unidade { "MyString" }
+    quantidade { 1 }
+    desconto { "9.99" }
+    valor { "9.99" }
+    aliquotaIPI { "9.99" }
+    descricao { "MyText" }
+    descricaoDetalhada { "MyText" }
+  end
+end
---
+++ spec/models/bling_order_item_spec.rb
@@ -0,0 +1,24 @@
+# == Schema Information
+#
+# Table name: bling_order_items
+#
+#  id                 :bigint           not null, primary key
+#  aliquotaIPI        :decimal(, )
+#  codigo             :string
+#  desconto           :decimal(, )
+#  descricao          :text
+#  descricaoDetalhada :text
+#  quantidade         :integer
+#  unidade            :string
+#  valor              :decimal(, )
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  bling_order_id     :string
+#  situation_id       :string
+#  store_id           :string
+#
+require 'rails_helper'
+
+RSpec.describe BlingOrderItem, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
