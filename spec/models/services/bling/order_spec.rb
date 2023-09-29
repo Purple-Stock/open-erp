41f13c258d11763ec9bf8ec5aae0dea0d424fb8d
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-09-29 18:45:47 -0300

    test order_spec.rb: describe #call when filtering by initial and final dates, it counts 1 and has initial_date equal to result date.

--- spec/models/services/bling/order_spec.rb
+++ spec/models/services/bling/order_spec.rb
@@ -13,4 +13,23 @@ RSpec.describe Services::Bling::Order, type: :services do
       expect(described_class.call(order_command: order_command, tenant: account_id, situation: situation)).to be_truthy
     end
   end
+
+  describe '#call' do
+    context 'when filtering by initial and final dates' do
+      let(:situation) { 9 }
+      let(:initial_date) { '2022-12-31' }
+      let(:end_date) { '2022-12-31' }
+      let(:options) { { dataInicial: initial_date, dataFinal: end_date } }
+
+      it 'is has data equal initial date' do
+        result = described_class.call(order_command: order_command, tenant: account_id, situation: situation, options: options)
+        expect(result['data'].dig(0).fetch('data')).to eq(initial_date)
+      end
+
+      it 'counts by 1' do
+        result = described_class.call(order_command: order_command, tenant: account_id, situation: situation, options: options)
+        expect(result['data'].count).to eq(1)
+      end
+    end
+  end
 end
