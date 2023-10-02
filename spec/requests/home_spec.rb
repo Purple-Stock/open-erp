7ff5faa392e8d2c3af865154e78b4ececb351324
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-10-02 19:54:58 -0300

    test spec/requests/home_spec.rb: when get root path by a logged user.

---
+++ spec/requests/home_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'orders control' do
+  context 'when get root_path' do
+    let(:user) { FactoryBot.create(:user) }
+
+    before do
+      FactoryBot.create(:bling_datum, account_id: user.account.id, expires_at: Time.now + 2.day)
+      sign_in user
+      get root_path
+    end
+
+    it 'is a successful response' do
+      expect(response).to be_successful
+    end
+  end
+end
