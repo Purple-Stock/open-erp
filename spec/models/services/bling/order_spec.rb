665464828b18c8580ee8967b6a0c574ceb9965c2
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-10-02 08:45:04 -0300

    lint order_spec.rb: solve too many memoized helpers.

--- spec/models/services/bling/order_spec.rb
+++ spec/models/services/bling/order_spec.rb
@@ -3,31 +3,28 @@
 require 'rails_helper'
 
 RSpec.describe Services::Bling::Order, type: :services do
-  let(:account_id) { 1 }
-  let!(:bling_datum) { FactoryBot.create(:bling_datum, account_id: account_id) }
+  let!(:bling_datum) { FactoryBot.create(:bling_datum, account_id: 1) }
   let(:order_command) { 'find_orders' }
   let(:situation) { 15 }
 
   describe '#initialize' do
     it 'is truthy' do
-      expect(described_class.call(order_command: order_command, tenant: account_id, situation: situation)).to be_truthy
+      expect(described_class.call(order_command: order_command, tenant: 1, situation: situation)).to be_truthy
     end
   end
 
   describe '#call' do
     context 'when filtering by initial and final dates' do
       let(:situation) { 9 }
-      let(:initial_date) { '2022-12-31' }
-      let(:end_date) { '2022-12-31' }
-      let(:options) { { dataInicial: initial_date, dataFinal: end_date } }
+      let(:options) { { dataInicial: '2022-12-31', dataFinal: '2022-12-31' } }
 
       it 'is has data equal initial date' do
-        result = described_class.call(order_command: order_command, tenant: account_id, situation: situation, options: options)
-        expect(result['data'].dig(0).fetch('data')).to eq(initial_date)
+        result = described_class.call(order_command: order_command, tenant: 1, situation: situation, options: options)
+        expect(result['data'].dig(0).fetch('data')).to eq('2022-12-31')
       end
 
       it 'counts by 1' do
-        result = described_class.call(order_command: order_command, tenant: account_id, situation: situation, options: options)
+        result = described_class.call(order_command: order_command, tenant: 1, situation: situation, options: options)
         expect(result['data'].count).to eq(1)
       end
     end
