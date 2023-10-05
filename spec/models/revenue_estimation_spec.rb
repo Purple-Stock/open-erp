4969f06e32ae30d246125018b01e81b91ecc9acf
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-10-05 20:40:33 -0300

    chore revenue_estimation_spec.rb: issue #259 Add Schema information

--- spec/models/revenue_estimation_spec.rb
+++ spec/models/revenue_estimation_spec.rb
@@ -1,5 +1,17 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: revenue_estimations
+#
+#  id             :bigint           not null, primary key
+#  average_ticket :decimal(, )
+#  date           :date
+#  quantity       :integer
+#  revenue        :decimal(, )
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe RevenueEstimation, type: :model do
