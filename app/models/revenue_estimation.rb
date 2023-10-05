3ca89294e1d9024f05b46f8f689a803feb9e0ed6
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-10-05 20:40:52 -0300

    chore revenue_estimation.rb: issue #259 Add Schema information

--- app/models/revenue_estimation.rb
+++ app/models/revenue_estimation.rb
@@ -1,5 +1,17 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: revenue_estimations
+#
+#  id             :bigint           not null, primary key
+#  average_ticket :decimal(, )
+#  date           :date
+#  quantity       :integer
+#  revenue        :decimal(, )
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
 # RevenueEstimation exists to answer a question:
 # How much Order Items to sale in order to achieve the average ticket
 # for the given month?
