f14ff55f5f7d6406b7df2d476312148a79009841
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-10-05 20:40:00 -0300

    chore factories/revenue_estimations.rb: issue #259 Add date to the factory definition.

--- spec/factories/revenue_estimations.rb
+++ spec/factories/revenue_estimations.rb
@@ -1,7 +1,20 @@
+# == Schema Information
+#
+# Table name: revenue_estimations
+#
+#  id             :bigint           not null, primary key
+#  average_ticket :decimal(, )
+#  date           :date
+#  quantity       :integer
+#  revenue        :decimal(, )
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
 FactoryBot.define do
   factory :revenue_estimation do
     average_ticket { 9.99 }
     quantity { 1 }
     revenue { 9.99 }
+    date { Date.today }
   end
 end
