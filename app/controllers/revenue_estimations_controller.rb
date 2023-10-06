43f5591b484d426ab39a3be25f3530aabe50f79b
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-10-06 10:14:45 -0300

    feat revenue_estimations_controller.rb: issue #259 Add inherited_resource all actions.

--- app/controllers/revenue_estimations_controller.rb
+++ app/controllers/revenue_estimations_controller.rb
@@ -1,5 +1,11 @@
 class RevenueEstimationsController < ApplicationController
-  def index
-    @revenue_estimations = RevenueEstimation.all
+  inherit_resources
+
+  private
+
+  def resource_params
+    return [] if request.get?
+
+    params.require(:revenue_estimation).permit(:date, :average_ticket, :revenue, :quantity)
   end
 end
