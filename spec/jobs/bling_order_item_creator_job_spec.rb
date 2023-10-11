6f1830599618bcb17adb11545f98f1fd101c99f1
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-10-11 12:17:26 -0300

    test bling_order_item_creator_job_spec.rb: fix counts for new requests.

--- spec/jobs/bling_order_item_creator_job_spec.rb
+++ spec/jobs/bling_order_item_creator_job_spec.rb
@@ -5,35 +5,36 @@ RSpec.describe BlingOrderItemCreatorJob, type: :job do
 
   describe '#create_in_progress_order_items' do
     before do
+      BlingOrderItem.destroy_all
       subject.account_id = user.account.id
       FactoryBot.create(:bling_datum, account_id: user.account.id, expires_at: Time.now + 2.day)
     end
 
-    it 'counts by 143 bling order items' do
-      VCR.use_cassette('in_progress_oder_items_situation', erb: true) do
+    it 'counts by 63 bling order items' do
+      VCR.use_cassette('in_progress_order_items_situation', erb: true) do
         expect do
           subject.send(:create_in_progress_order_items)
-        end.to change(BlingOrderItem, :count).by(1)
+        end.to change(BlingOrderItem, :count).by(63)
       end
     end
 
-    it 'counts by 0 checked bling order items' do
+    it 'counts by 202 checked bling order items' do
       VCR.use_cassette('checked_order_items_situation', erb: true) do
         expect do
           subject.send(:create_checked_order_items)
-        end.to change(BlingOrderItem, :count).by(0)
+        end.to change(BlingOrderItem, :count).by(202)
       end
     end
 
-    it 'counts by 0 pending bling order items' do
+    it 'counts by 204 pending bling order items' do
       VCR.use_cassette('pending_order_items_situation', erb: true) do
         expect do
           subject.send(:create_pending_order_items)
-        end.to change(BlingOrderItem, :count).by(0)
+        end.to change(BlingOrderItem, :count).by(204)
       end
     end
 
-    it 'counts by 8 printed bling order items' do
+    it 'counts by 212 printed bling order items' do
       VCR.use_cassette('printed_order_items_situation', erb: true) do
         expect do
           subject.send(:create_printed_order_items)
