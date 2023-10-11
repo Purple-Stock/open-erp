1aed2887472b0d550ed46604bff5fd96ad91f2c7
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-10-11 20:20:34 -0300

    test bling_order_item_creator_job_spec.rb: describe perform "now".

--- spec/jobs/bling_order_item_creator_job_spec.rb
+++ spec/jobs/bling_order_item_creator_job_spec.rb
@@ -1,7 +1,25 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe BlingOrderItemCreatorJob, type: :job do
   let(:user) { FactoryBot.create(:user) }
+
+  describe '#perform_now' do
+    before do
+      BlingOrderItem.destroy_all
+      subject.account_id = user.account.id
+      FactoryBot.create(:bling_datum, account_id: user.account.id, expires_at: Time.now + 2.day)
+    end
+
+    it 'counts by 722 bling order items' do
+      VCR.use_cassette('all_situations_bling_order_items', erb: true) do
+        expect do
+          subject.perform(user.account.id)
+        end.to change(BlingOrderItem, :count).by(722)
+      end
+    end
+  end
 
   describe '#create_in_progress_order_items' do
     before do
