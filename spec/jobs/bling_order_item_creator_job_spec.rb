b6dbace62a10b167e7ea74c6fd7d4fd8d262a655
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-10-11 22:02:01 -0300

    refactor bling_order_item_creator_job_spec.rb: mock requests for all situations version using a single situation. Indeed we just need to make sure a single status is inserted into the database. The others are a mathematical same proof.

--- spec/jobs/bling_order_item_creator_job_spec.rb
+++ spec/jobs/bling_order_item_creator_job_spec.rb
@@ -9,13 +9,14 @@ RSpec.describe BlingOrderItemCreatorJob, type: :job do
     before do
       BlingOrderItem.destroy_all
       FactoryBot.create(:bling_datum, account_id: user.account.id, expires_at: Time.now + 2.day)
+      allow(subject).to receive(:list_status_situation).and_return([15])
     end
 
-    it 'counts by 722 bling order items' do
+    it 'counts by 129 bling order items' do
       VCR.use_cassette('all_situations_bling_order_items', erb: true) do
         expect do
           subject.perform(user.account.id)
-        end.to change(BlingOrderItem, :count).by(722)
+        end.to change(BlingOrderItem, :count).by(129)
       end
     end
   end
