4a1fa56a99b69bdd1578c25bfbe83d3b1e2a366d
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-10-16 16:26:42 -0300

    feat home_controller.rb: issue #278 get in progress order items before render index. Also remove call to bling api asking items whose status is in progress 15.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
-  before_action :set_monthly_revenue_estimation, only: :index
+  before_action :set_monthly_revenue_estimation, :get_in_progress_order_items, only: :index
   include SheinOrdersHelper
+
+  def get_in_progress_order_items
+    @in_progress_order_items = BlingOrderItem.where(situation_id: 15)
+  end
 
   def index
     @current_order_items = BlingOrderItem.where(situation_id: %w[15 101065 24 94871 95745])
@@ -9,9 +13,9 @@ class HomeController < ApplicationController
 
     refresh_token if date_expires < DateTime.now && Rails.env.eql?('production')
 
-    @shein_orders_count = SheinOrder.where("data ->> 'Status do pedido' IN (?)", ['A ser coletado pela SHEIN'])                                
+    @shein_orders_count = SheinOrder.where("data ->> 'Status do pedido' IN (?)", ['A ser coletado pela SHEIN'])
                                     .count
-    
+
     @shein_orders = SheinOrder.where("data ->> 'Status do pedido' IN (?)", ['A ser coletado pela SHEIN', 'Pendente', 'Para ser enviado'])
     @expired_orders = @shein_orders.select { |order| order_status(order) == "Atrasado" }
     @expired_orders_count = @expired_orders.count
