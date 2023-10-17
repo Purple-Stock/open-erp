7e0f20251b7d36a15ac91f2e5fdc67600cc868f0
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-10-17 10:10:14 -0300

    test current_done_bling_order_item_job_spec.rb: issue #261 Add describe #perform_now

---
+++ spec/jobs/current_done_bling_order_item_job_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe CurrentDoneBlingOrderItemJob, type: :job do
+  let(:user) { FactoryBot.create(:user) }
+
+  describe '#perform_now' do
+    before do
+      allow(Date).to receive(:today).and_return Date.new(2023, 10, 13)
+      BlingOrderItem.destroy_all
+      FactoryBot.create(:bling_datum, account_id: user.account.id, expires_at: Time.now + 2.day)
+    end
+
+    it 'counts by 191 bling order items' do
+      VCR.use_cassette('verified_checked_order_items_situation', erb: true) do
+        expect do
+          subject.perform(user.account.id)
+        end.to change(BlingOrderItem, :count).by(191)
+      end
+    end
+  end
+end
