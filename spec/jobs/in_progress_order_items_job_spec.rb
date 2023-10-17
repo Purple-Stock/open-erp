bb83ac35fa1ee4853b203e28f63712280a9bd59d
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-10-17 19:27:32 -0300

    test in_progress_order_items_job_spec.rb: issue #261 describe perform

---
+++ spec/jobs/in_progress_order_items_job_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe InProgressOrderItemsJob, type: :job do
+  let(:user) { FactoryBot.create(:user) }
+
+  describe '#perform_now' do
+    before do
+      BlingOrderItem.destroy_all
+      FactoryBot.create(:bling_datum, account_id: user.account.id, expires_at: Time.now + 2.day)
+    end
+
+    it 'counts by 191 bling order items' do
+      VCR.use_cassette('all_in_progress_order_items', erb: true) do
+        expect do
+          subject.perform(user.account.id)
+        end.to change(BlingOrderItem, :count).by(123)
+      end
+    end
+  end
+end
