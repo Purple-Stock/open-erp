a9e199d1ccf8b432b3b0224ab5e4093abc9827ea
puppe1990 <matheus.puppe90@hotmail.com>
2023-10-18 11:23:05 -0300

    fix

--- app/helpers/shein_orders_helper.rb
+++ app/helpers/shein_orders_helper.rb
@@ -2,10 +2,10 @@
 
 module SheinOrdersHelper
   def time_remaining(shein_order)
-    collection_deadline = DateTime.parse(shein_order.data['Limite de tempo para coletar']) rescue nil
+    collection_deadline = parse_portuguese_date(shein_order.data['Limite de tempo para coletar'])
     
     if collection_deadline
-      time_remaining = collection_deadline - DateTime.now
+      time_remaining = collection_deadline - Time.zone.now.to_datetime
       days_remaining = time_remaining.to_i
       hours_remaining = (time_remaining % 1 * 24).to_i
       minutes_remaining = (time_remaining * 24 % 1 * 60).to_i
@@ -20,13 +20,47 @@ module SheinOrdersHelper
   end
 
   def order_status(shein_order)
-    collection_deadline = DateTime.parse(shein_order.data['Limite de tempo para coletar']) rescue nil
+    collection_deadline = parse_portuguese_date(shein_order.data['Limite de tempo para coletar'])
     
     if collection_deadline
-      time_remaining = collection_deadline - DateTime.now
+      time_remaining = collection_deadline - Time.zone.now.to_datetime
       time_remaining.negative? ? "Atrasado" : "Em dia"
     else
       "Invalid Date"
     end
+  end
+
+  private
+
+  def parse_portuguese_date(portuguese_date_string)
+    # Define a hash to map Portuguese month names to English
+    month_translation = {
+      'janeiro' => 'January',
+      'fevereiro' => 'February',
+      'março' => 'March',
+      'abril' => 'April',
+      'maio' => 'May',
+      'junho' => 'June',
+      'julho' => 'July',
+      'agosto' => 'August',
+      'setembro' => 'September',
+      'outubro' => 'October',
+      'novembro' => 'November',
+      'dezembro' => 'December'
+    }
+
+    # Split the input Portuguese date string into day, month, year, and time parts
+    parts = portuguese_date_string.split
+
+    # Translate the Portuguese month name to English
+    english_month = month_translation[parts[1]]
+
+    # Create a new date string with the English month name
+    english_date_string = "#{parts[0]} #{english_month} #{parts[2]} #{parts[3]}"
+
+    # Parse the English date string into a DateTime object
+    DateTime.parse(english_date_string)
+  rescue
+    nil
   end
 end
