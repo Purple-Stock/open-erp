aa8d93ad12092d2f8cffc9f4d4f294b9673d9a8d
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-10-20 13:43:20 -0300

    test current_done_bling_order_item_job_spec.rb: issue #261 It counts 320 items has alteration day equal to 20 and grouped by bling order id counts by 308. Those checks are necessary to make sure we are counting correctly.

--- spec/jobs/current_done_bling_order_item_job_spec.rb
+++ spec/jobs/current_done_bling_order_item_job_spec.rb
@@ -7,16 +7,32 @@ RSpec.describe CurrentDoneBlingOrderItemJob, type: :job do
 
   describe '#perform_now' do
     before do
-      allow(Date).to receive(:today).and_return Date.new(2023, 10, 13)
+      allow(Rails).to receive(:env).and_return('no_test')
       BlingOrderItem.destroy_all
       FactoryBot.create(:bling_datum, account_id: user.account.id, expires_at: Time.now + 2.day)
     end
 
-    xit 'counts by 191 bling order items' do
+    it 'counts by 320 bling order items' do
       VCR.use_cassette('verified_checked_order_items_situation', erb: true) do
         expect do
           subject.perform(user.account.id)
-        end.to change(BlingOrderItem, :count).by(191)
+        end.to change(BlingOrderItem, :count).by(320)
+      end
+    end
+
+    it 'counts by 308 grouped by bling_order_id' do
+      VCR.use_cassette('verified_checked_order_items_situation', erb: true) do
+        subject.perform(user.account.id)
+
+        expect(BlingOrderItem.all.group_by(&:bling_order_id).keys.length).to eq(308)
+      end
+    end
+
+    it 'has alteration_date day equal to 20' do
+      VCR.use_cassette('verified_checked_order_items_situation', erb: true) do
+        subject.perform(user.account.id)
+
+        expect(BlingOrderItem.first.alteration_date.day).to eq(20)
       end
     end
   end
