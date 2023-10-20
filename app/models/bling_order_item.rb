746db943c20d0b42c2f6097343ee0ba37442c156
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-10-20 13:47:30 -0300

    chore bling_order_item.rb, bling_order_item_spec.rb, bling_order_items.rb: issue #261 Add table definitions.

--- app/models/bling_order_item.rb
+++ app/models/bling_order_item.rb
@@ -4,6 +4,7 @@
 #
 #  id                 :bigint           not null, primary key
 #  aliquotaIPI        :decimal(, )
+#  alteration_date    :datetime
 #  codigo             :string
 #  date               :datetime
 #  desconto           :decimal(, )
