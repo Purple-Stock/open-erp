d9c64b3f2aa6072c93081a9d7a8436b47805270d
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-10-23 17:24:45 -0300

    test current_done_bling_order_item_job_spec.rb.rb: issue #261 Add context to interact with a present pending bling order item. When it is present, update its situation to checked rather than creating a new bling order item.

--- spec/jobs/current_done_bling_order_item_job_spec.rb
+++ spec/jobs/current_done_bling_order_item_job_spec.rb
@@ -13,6 +13,35 @@ RSpec.describe CurrentDoneBlingOrderItemJob, type: :job do
       FactoryBot.create(:bling_datum, account_id: user.account.id, expires_at: Time.now + 2.day)
     end
 
+    context 'when there is a pending bling order item in local database' do
+      before do
+        FactoryBot.create(:bling_order_item, bling_order_id: 18969858161,
+                                             situation_id: BlingOrderItem::Status::PENDING)
+      end
+
+      it 'counts by 319 bling order items' do
+        VCR.use_cassette('verified_checked_order_items_situation', erb: true) do
+          expect do
+            subject.perform(user.account.id)
+          end.to change(BlingOrderItem, :count).by(319)
+        end
+      end
+
+      it 'has alteration date present' do
+        VCR.use_cassette('verified_checked_order_items_situation', erb: true) do
+          subject.perform(user.account.id)
+          expect(BlingOrderItem.first.alteration_date).to be_present
+        end
+      end
+
+      it 'has situation changed to checked' do
+        VCR.use_cassette('verified_checked_order_items_situation', erb: true) do
+          subject.perform(user.account.id)
+          expect(BlingOrderItem.first.situation_id.to_i).to eq(BlingOrderItem::Status::CHECKED)
+        end
+      end
+    end
+
     it 'counts by 320 bling order items' do
       VCR.use_cassette('verified_checked_order_items_situation', erb: true) do
         expect do
