b69c5f248438c29c517bb2740da221178b694eae
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-11-05 17:04:53 -0300

    test bling_order_item_histories_spec.rb: task #338 fix request test sign in user.

--- spec/requests/bling_order_item_histories_spec.rb
+++ spec/requests/bling_order_item_histories_spec.rb
@@ -1,11 +1,16 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe "BlingOrderItemHistories", type: :request do
+  let(:user) { FactoryBot.create(:user) }
+
+  before { sign_in user }
+
   describe "GET /index" do
     it "returns http success" do
-      get "/bling_order_item_histories/index"
+      get bling_order_item_histories_path
       expect(response).to have_http_status(:success)
     end
   end
-
 end
