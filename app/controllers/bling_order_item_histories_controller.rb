a328753ebbab5dbeea657c9113d9af25cc018dea
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-11-06 11:57:23 -0300

    feat bling_order_item_histories_controller.rb: task #338 render day quantity according to frontend necessity.

--- app/controllers/bling_order_item_histories_controller.rb
+++ app/controllers/bling_order_item_histories_controller.rb
@@ -1,5 +1,23 @@
+# frozen_string_literal: true
+
+# There is a necessity to know the revenue from the last 15 days
+# In order to take better commercial decisions.
 class BlingOrderItemHistoriesController < ApplicationController
-  def index
-    @bling_order_items = BlingOrderItem.all
+  def index;end
+
+  def day_quantities
+    # TODO, separate responsibilities: the presenter pattern
+    initial_date = (Date.today - 15.days).beginning_of_day
+    final_date = Date.today.end_of_day
+    date_range = initial_date..final_date
+    @paid_bling_order_items = BlingOrderItem.where(date: date_range, situation_id: [BlingOrderItem::Status::PAID])
+                                            .group(:date).count
+
+
+    result = @paid_bling_order_items.map do |day_quantity|
+      { day: day_quantity.dig(0).day, quantity: day_quantity.dig(1) }
+    end
+
+    render json: result
   end
 end
