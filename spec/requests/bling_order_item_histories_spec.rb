71e3be7434c7f5f8ae61358027226d4a66cda111
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-11-06 17:12:53 -0300

    test bling_order_item_histories_spec.rb: task #338 add context when there is no data in the collection.

--- spec/requests/bling_order_item_histories_spec.rb
+++ spec/requests/bling_order_item_histories_spec.rb
@@ -2,28 +2,38 @@
 
 require 'rails_helper'
 
-RSpec.describe "BlingOrderItemHistories", type: :request do
+RSpec.describe 'BlingOrderItemHistories', type: :request do
   let(:user) { FactoryBot.create(:user) }
 
   before { sign_in user }
 
-  describe "GET /index" do
-    it "returns http success" do
+  describe 'GET /index' do
+    it 'returns http success' do
       get bling_order_item_histories_path
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /day_quantity" do
-    before do
-      FactoryBot.create_list(:bling_order_item, 2, store_id: BlingOrderItem::STORE_ID_NAME_KEY_VALUE['Shopee'])
-      FactoryBot.create_list(:bling_order_item, 2, date: Date.today - 2.days, bling_order_id: '2')
+  describe 'GET /day_quantity' do
+    context 'when there is not data in collection' do
+      it 'returns http success' do
+        get day_quantities_bling_order_item_histories_path
+
+        expect(response).to have_http_status(:success)
+      end
     end
 
-    it "returns http success" do
-      get day_quantities_bling_order_item_histories_path
+    context 'when there is data in collection' do
+      before do
+        FactoryBot.create_list(:bling_order_item, 2, store_id: BlingOrderItem::STORE_ID_NAME_KEY_VALUE['Shopee'])
+        FactoryBot.create_list(:bling_order_item, 2, date: Date.today - 2.days, bling_order_id: '2')
+      end
 
-      expect(response).to have_http_status(:success)
+      it 'returns http success' do
+        get day_quantities_bling_order_item_histories_path
+
+        expect(response).to have_http_status(:success)
+      end
     end
   end
 end
