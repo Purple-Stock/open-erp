ea749d29a7f533a940c41af1a1286ee6581aa124
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-11-06 16:52:30 -0300

    feat bling_order_item_histories_presenter.rb: task #338 Add class specialized in presentable.

---
+++ app/presenters/bling_order_item_histories_presenter.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# BlingOrderItemHistoriesPresentable returns the hash the Chart js needs.
+class BlingOrderItemHistoriesPresenter
+  attr_accessor :bling_order_item_collection
+
+  def initialize(bling_order_item_collection)
+    @bling_order_item_collection = bling_order_item_collection
+  end
+
+  def presentable
+    bling_order_item_collection.count.map do |day_quantity|
+      { day: day_quantity[0].day, quantity: day_quantity[1] }
+    end
+  end
+end
