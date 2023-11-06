66d235f00023a5a03eee38fb7510f2493b28cd8c
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-11-06 17:41:51 -0300

    test bling_order_item_histories_presenter_spec.rb: task #338 add context when there is data in the collection and when there is not.

--- spec/presenters/bling_order_item_histories_presenter_spec.rb
+++ spec/presenters/bling_order_item_histories_presenter_spec.rb
@@ -12,7 +12,22 @@ RSpec.describe BlingOrderItemHistoriesPresenter do
   describe '#presentable' do
     context 'when there is not collection' do
       it 'is empty' do
-        expect(described_class.new.bling_order_item_collection).to be_empty
+        empty_bling_order_item_collection = BlingOrderItem.all
+        expect(described_class.new(empty_bling_order_item_collection).presentable).to be_empty
+      end
+    end
+
+    context 'when there is data in the collection' do
+      before do
+        allow(Date).to receive(:today).and_return Date.new(2023, 11, 6)
+        FactoryBot.create_list(:bling_order_item, 2, store_id: BlingOrderItem::STORE_ID_NAME_KEY_VALUE['Shopee'])
+        FactoryBot.create_list(:bling_order_item, 2, date: Date.today - 2.days, bling_order_id: '2')
+      end
+
+      it 'is an array of hash' do
+        result = [{ day: 4, quantity: 2 }, { day: 6, quantity: 2 }]
+        bling_order_item_collection = BlingOrderItem.all
+        expect(described_class.new(bling_order_item_collection).presentable).to eq(result)
       end
     end
   end
