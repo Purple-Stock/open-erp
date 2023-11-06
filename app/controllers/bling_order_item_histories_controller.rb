5c6289e5344fa80c4e5545f25f954cbb86b7a454
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-11-06 16:36:56 -0300

    refactor bling_order_item_histories_controller.rb: task #338 move paid bling order items to instance method and prepare to a presenter pattern.

--- app/controllers/bling_order_item_histories_controller.rb
+++ app/controllers/bling_order_item_histories_controller.rb
@@ -3,21 +3,30 @@
 # There is a necessity to know the revenue from the last 15 days
 # In order to take better commercial decisions.
 class BlingOrderItemHistoriesController < ApplicationController
+  before_action :date_range, :paid_bling_order_items, :day_quantities_presenter, only: :day_quantities
+
   def index;end
 
   def day_quantities
-    # TODO, separate responsibilities: the presenter pattern
+    render json: @bling_order_item_day_quantities_presenter
+  end
+
+  private
+
+  def date_range
     initial_date = (Date.today - 15.days).beginning_of_day
     final_date = Date.today.end_of_day
-    date_range = initial_date..final_date
+    @date_range = initial_date..final_date
+  end
+
+  def paid_bling_order_items
     @paid_bling_order_items = BlingOrderItem.where(date: date_range, situation_id: [BlingOrderItem::Status::PAID])
-                                            .group(:date).count
+                                            .group(:date)
+  end
 
-
-    result = @paid_bling_order_items.map do |day_quantity|
+  def day_quantities_presenter
+    @bling_order_item_day_quantities_presenter = @paid_bling_order_items.count.map do |day_quantity|
       { day: day_quantity.dig(0).day, quantity: day_quantity.dig(1) }
     end
-
-    render json: result
   end
 end
