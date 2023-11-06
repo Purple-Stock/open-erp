37c99257cbf59dabee4022ec953e3d31fc538d72
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-11-06 17:43:28 -0300

    fix bling_order_item_histories_presenter.rb.rb: task #338 initialize raise argument error when not passing any bling order item collection. Fix group to an empty array.

--- app/presenters/bling_order_item_histories_presenter.rb
+++ app/presenters/bling_order_item_histories_presenter.rb
@@ -4,12 +4,12 @@
 class BlingOrderItemHistoriesPresenter
   attr_accessor :bling_order_item_collection
 
-  def initialize(bling_order_item_collection = [])
+  def initialize(bling_order_item_collection)
     @bling_order_item_collection = bling_order_item_collection
   end
 
   def presentable
-    bling_order_item_collection.count.map do |day_quantity|
+    bling_order_item_collection.group(:date).count.map do |day_quantity|
       { day: day_quantity[0].day, quantity: day_quantity[1] }
     end
   end
