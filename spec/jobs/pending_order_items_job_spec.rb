8163ed637964ecba476432d22fe5366fcfb3e164
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-11-08 19:47:44 -0300

    test pending_order_items_job_spec.rb: task #325 where there are or not pending orders. count by orders, not by items anymore.

--- spec/jobs/pending_order_items_job_spec.rb
+++ spec/jobs/pending_order_items_job_spec.rb
@@ -11,11 +11,33 @@ RSpec.describe PendingOrderItemsJob, type: :job do
       FactoryBot.create(:bling_datum, account_id: user.account.id, expires_at: Time.now + 2.day)
     end
 
-    it 'counts by 110 bling order items' do
+    xit 'counts by 110 bling order items' do
       VCR.use_cassette('all_pending_order_items', erb: true) do
         expect do
           subject.perform(user.account.id)
         end.to change(BlingOrderItem, :count).by(110)
+      end
+    end
+
+    context 'when there is no pending orders' do
+      it 'counts by 100 bling order items' do
+        VCR.use_cassette('all_pending_order_items', erb: true) do
+          expect do
+            subject.perform(user.account.id)
+          end.to change(BlingOrderItem, :count).by(100)
+        end
+      end
+    end
+
+    context 'when there are pending orders' do
+      before { subject.perform(user.account.id) }
+
+      it 'counts by 0 bling order items' do
+        VCR.use_cassette('all_pending_order_items', erb: true) do
+          expect do
+            subject.perform(user.account.id)
+          end.to change(BlingOrderItem, :count).by(0)
+        end
       end
     end
   end
