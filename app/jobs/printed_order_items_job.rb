a6ad4b66e650cd1ef4c19b308ad123ec14172972
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-11-08 21:11:54 -0300

    refactor printed_order_items_job.rb: task #325 bulk create version on repeating order id skip.

--- app/jobs/printed_order_items_job.rb
+++ app/jobs/printed_order_items_job.rb
@@ -16,4 +16,23 @@ class PrintedOrderItemsJob < BlingOrderItemCreatorBaseJob
       Rails.logger.error(e.message)
     end
   end
+
+  def create_orders(orders)
+    orders_attributes = []
+    query_order_ids = BlingOrderItem.all.pluck(:bling_order_id).map(&:to_i)
+
+    orders.each do |order|
+      next if query_order_ids.include?(order['id'])
+
+      orders_attributes << {
+        bling_order_id: order['id'],
+        situation_id: order['situacao']['id'],
+        store_id: order['loja']['id'],
+        date: order['data'],
+        alteration_date:
+      }
+    end
+
+    BlingOrderItem.create!(orders_attributes)
+  end
 end
