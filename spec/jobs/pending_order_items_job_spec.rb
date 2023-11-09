8dc0c70c4a188a5ea7c00240460e0167d274b694
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-11-09 20:03:11 -0300

    refactor pending_order_items_job_spec.rb: task #325 remove skipped test.

--- spec/jobs/pending_order_items_job_spec.rb
+++ spec/jobs/pending_order_items_job_spec.rb
@@ -11,14 +11,6 @@ RSpec.describe PendingOrderItemsJob, type: :job do
       FactoryBot.create(:bling_datum, account_id: user.account.id, expires_at: Time.now + 2.day)
     end
 
-    xit 'counts by 110 bling order items' do
-      VCR.use_cassette('all_pending_order_items', erb: true) do
-        expect do
-          subject.perform(user.account.id)
-        end.to change(BlingOrderItem, :count).by(110)
-      end
-    end
-
     context 'when there is no pending orders' do
       it 'counts by 100 bling order items' do
         VCR.use_cassette('all_pending_order_items', erb: true) do
