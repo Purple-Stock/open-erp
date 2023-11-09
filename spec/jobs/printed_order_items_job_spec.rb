36e46af6ba5c586b0cdd3f5f5f6cbc2b809cb1d8
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-11-08 21:09:13 -0300

    refactor printed_order_items_job_spec.rb: task #325 bulk create version on repeating order id skip.

--- spec/jobs/printed_order_items_job_spec.rb
+++ spec/jobs/printed_order_items_job_spec.rb
@@ -11,11 +11,27 @@ RSpec.describe PrintedOrderItemsJob, type: :job do
       FactoryBot.create(:bling_datum, account_id: user.account.id, expires_at: Time.now + 2.day)
     end
 
-    it 'counts by 100 bling order items' do
-      VCR.use_cassette('all_printed_order_items', erb: true) do
-        expect do
-          subject.perform(user.account.id)
-        end.to change(BlingOrderItem, :count).by(100)
+    context 'when there is no printed orders' do
+      it 'counts by 100 bling order items' do
+        VCR.use_cassette('all_printed_order_items', erb: true) do
+          expect do
+            subject.perform(user.account.id)
+          end.to change(BlingOrderItem, :count).by(100)
+        end
+      end
+    end
+
+    context 'when there are printed orders' do
+      before do
+        FactoryBot.create(:bling_order_item, bling_order_id: '18970670379')
+      end
+
+      it 'counts by 99 bling order items' do
+        VCR.use_cassette('all_printed_order_items', erb: true) do
+          expect do
+            subject.perform(user.account.id)
+          end.to change(BlingOrderItem, :count).by(99) # one already created.
+        end
       end
     end
   end
