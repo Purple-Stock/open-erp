75c7eed0f34a6a5824763e82444092e95be2ecfd
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-11-09 18:56:17 -0300

    refactor current_done_bling_order_item_job.rb: task #325 bulk create version. It needs dealing with alteration date for existent orders.

--- app/jobs/current_done_bling_order_item_job.rb
+++ app/jobs/current_done_bling_order_item_job.rb
@@ -10,7 +10,7 @@ class CurrentDoneBlingOrderItemJob < BlingOrderItemCreatorBaseJob
     begin
       DONE_STATUSES.each do |status|
         orders = Services::Bling::Order.call(order_command: 'find_orders', tenant: account_id,
-                                             situation: status, options: options)
+                                             situation: status, options:)
         orders = orders['data']
 
         create_orders(orders)
@@ -18,5 +18,31 @@ class CurrentDoneBlingOrderItemJob < BlingOrderItemCreatorBaseJob
     rescue StandardError => e
       Rails.logger.error(e.message)
     end
+  end
+
+  def create_orders(orders)
+    return if orders.blank?
+
+    order_ids = orders.map { |order| order['id'] }
+    query_bling_order_ids = BlingOrderItem.where(bling_order_id: order_ids)
+                                          .pluck(:bling_order_id)
+                                          .map(&:to_i)
+
+
+    orders_attributes = []
+
+    orders.each do |order|
+      next if query_bling_order_ids.include?(order['id'])
+
+      orders_attributes << {
+        bling_order_id: order['id'],
+        situation_id: order['situacao']['id'],
+        store_id: order['loja']['id'],
+        date: order['data'],
+        alteration_date:
+      }
+    end
+
+    BlingOrderItem.create(orders_attributes)
   end
 end
