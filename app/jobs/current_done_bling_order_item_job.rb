e6fefaa90da2bf2cec0ab70280583e2f7c9da9e7
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-11-09 21:51:30 -0300

    refactor current_done_bling_order_item_job.rb: task #325 bulk create and update all version inherited from parent.

--- app/jobs/current_done_bling_order_item_job.rb
+++ app/jobs/current_done_bling_order_item_job.rb
@@ -9,6 +9,7 @@ class CurrentDoneBlingOrderItemJob < BlingOrderItemCreatorBaseJob
     @account_id = account_id
     begin
       DONE_STATUSES.each do |status|
+        @status = status
         orders = Services::Bling::Order.call(order_command: 'find_orders', tenant: account_id,
                                              situation: status, options:)
         orders = orders['data']
@@ -18,33 +19,5 @@ class CurrentDoneBlingOrderItemJob < BlingOrderItemCreatorBaseJob
     rescue StandardError => e
       Rails.logger.error(e.message)
     end
-  end
-
-  def create_orders(orders)
-    return if orders.blank?
-
-    order_ids = orders.map { |order| order['id'] }
-    query_bling_order_ids = BlingOrderItem.where(bling_order_id: order_ids)
-                                          .pluck(:bling_order_id)
-                                          .map(&:to_i)
-
-
-    orders_attributes = []
-    BlingOrderItem.where(bling_order_id: [query_bling_order_ids])
-                  .update_all(alteration_date: @alteration_date, situation_id: BlingOrderItem::Status::CHECKED)
-
-    orders.each do |order|
-      next if query_bling_order_ids.include?(order['id'])
-
-      orders_attributes << {
-        bling_order_id: order['id'],
-        situation_id: order['situacao']['id'],
-        store_id: order['loja']['id'],
-        date: order['data'],
-        alteration_date:
-      }
-    end
-
-    BlingOrderItem.create(orders_attributes)
   end
 end
