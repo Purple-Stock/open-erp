0d34b9886058683eb934b546bc9ddfddb6b64b2f
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-11-09 21:52:36 -0300

    refactor in_progress_order_items_job.rb, printed_order_items_job.rb, pending_order_items_job.rb: task #325 bulk create and update all version inherited from parent.

--- app/jobs/in_progress_order_items_job.rb
+++ app/jobs/in_progress_order_items_job.rb
@@ -5,6 +5,7 @@ class InProgressOrderItemsJob < BlingOrderItemCreatorBaseJob
   attr_accessor :account_id
 
   def perform(account_id)
+    @status = STATUS
     @account_id = account_id
     begin
       orders = Services::Bling::Order.call(order_command: 'find_orders', tenant: account_id,
@@ -15,33 +16,5 @@ class InProgressOrderItemsJob < BlingOrderItemCreatorBaseJob
     rescue StandardError => e
       Rails.logger.error(e.message)
     end
-  end
-
-  def create_orders(orders)
-    return if orders.blank?
-
-    order_ids = orders.map { |order| order['id'] }
-    query_bling_order_ids = BlingOrderItem.where(bling_order_id: order_ids)
-                                          .pluck(:bling_order_id)
-                                          .map(&:to_i)
-
-
-    orders_attributes = []
-    BlingOrderItem.where(bling_order_id: [query_bling_order_ids])
-                  .update_all(situation_id: STATUS)
-
-    orders.each do |order|
-      next if query_bling_order_ids.include?(order['id'])
-
-      orders_attributes << {
-        bling_order_id: order['id'],
-        situation_id: order['situacao']['id'],
-        store_id: order['loja']['id'],
-        date: order['data'],
-        alteration_date:
-      }
-    end
-
-    BlingOrderItem.create(orders_attributes)
   end
 end
--- app/jobs/printed_order_items_job.rb
+++ app/jobs/printed_order_items_job.rb
@@ -5,34 +5,16 @@ class PrintedOrderItemsJob < BlingOrderItemCreatorBaseJob
   attr_accessor :account_id
 
   def perform(account_id)
+    @status = STATUS
     @account_id = account_id
     begin
       orders = Services::Bling::Order.call(order_command: 'find_orders', tenant: account_id,
-                                           situation: STATUS)
+                                           situation: @status)
       orders = orders['data']
 
       create_orders(orders)
     rescue StandardError => e
       Rails.logger.error(e.message)
     end
-  end
-
-  def create_orders(orders)
-    orders_attributes = []
-    query_order_ids = BlingOrderItem.all.pluck(:bling_order_id).map(&:to_i)
-
-    orders.each do |order|
-      next if query_order_ids.include?(order['id'])
-
-      orders_attributes << {
-        bling_order_id: order['id'],
-        situation_id: order['situacao']['id'],
-        store_id: order['loja']['id'],
-        date: order['data'],
-        alteration_date:
-      }
-    end
-
-    BlingOrderItem.create!(orders_attributes)
   end
 end
