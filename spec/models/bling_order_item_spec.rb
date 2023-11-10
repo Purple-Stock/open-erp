0179cfb442a5d5b9c631d4e94ce4eec7bc39f3fb
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-11-09 22:04:43 -0300

    feat add marketplace_code_id migration.rb: task #325

--- spec/models/bling_order_item_spec.rb
+++ spec/models/bling_order_item_spec.rb
@@ -2,22 +2,23 @@
 #
 # Table name: bling_order_items
 #
-#  id                 :bigint           not null, primary key
-#  aliquotaIPI        :decimal(, )
-#  alteration_date    :datetime
-#  codigo             :string
-#  date               :datetime
-#  desconto           :decimal(, )
-#  descricao          :text
-#  descricaoDetalhada :text
-#  quantidade         :integer
-#  unidade            :string
-#  valor              :decimal(, )
-#  created_at         :datetime         not null
-#  updated_at         :datetime         not null
-#  bling_order_id     :string
-#  situation_id       :string
-#  store_id           :string
+#  id                  :bigint           not null, primary key
+#  aliquotaIPI         :decimal(, )
+#  alteration_date     :datetime
+#  codigo              :string
+#  date                :datetime
+#  desconto            :decimal(, )
+#  descricao           :text
+#  descricaoDetalhada  :text
+#  quantidade          :integer
+#  unidade             :string
+#  valor               :decimal(, )
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  bling_order_id      :string
+#  marketplace_code_id :string
+#  situation_id        :string
+#  store_id            :string
 #
 # Indexes
 #
