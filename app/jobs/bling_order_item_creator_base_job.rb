1c10c06008fac5ef60ab66c632c5b7c3307d9d3f
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-11-09 21:50:41 -0300

    refactor bling_order_item_creator_base_job.rb: task #325 bulk create and update all version.

--- app/jobs/bling_order_item_creator_base_job.rb
+++ app/jobs/bling_order_item_creator_base_job.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class BlingOrderItemCreatorBaseJob < ApplicationJob
   queue_as :default
   attr_accessor :account_id, :alteration_date
@@ -12,44 +14,30 @@ class BlingOrderItemCreatorBaseJob < ApplicationJob
   end
 
   def create_orders(orders)
-    orders.each do |order_data|
-      order_id = order_data['id']
+    return if orders.blank?
 
-      if BlingOrderItem.exists?(bling_order_id: order_id)
-        bling_order_items = BlingOrderItem.where(bling_order_id: order_id)
-        if order_data['situacao']['id'] != bling_order_items[0].situation_id.to_i
-          @alteration_date = Date.today.strftime('%Y-%m-%d')
-          bling_order_items.each do |bling_order_item|
-            bling_order_item.update(situation_id: order_data['situacao']['id'], alteration_date: alteration_date)
-          end
-        end
-        next # Skip to the next order
-      end
+    order_ids = orders.map { |order| order['id'] }
+    query_bling_order_ids = BlingOrderItem.where(bling_order_id: order_ids)
+                                          .pluck(:bling_order_id)
+                                          .map(&:to_i)
 
-      fetched_order_data = fetch_order_data(order_id)
-      begin
-        fetched_order_data['data']['itens']
-      rescue
-        next
-      end
 
-      fetched_order_data['data']['itens'].each do |item_data|
-        BlingOrderItem.create!(
-          bling_order_id: order_id,
-          codigo: item_data['codigo'],
-          unidade: item_data['unidade'],
-          quantidade: item_data['quantidade'],
-          desconto: item_data['desconto'],
-          valor: item_data['valor'],
-          aliquotaIPI: item_data['aliquotaIPI'],
-          descricao: item_data['descricao'],
-          descricaoDetalhada: item_data['descricaoDetalhada'],
-          situation_id: fetched_order_data['data']['situacao']['id'],
-          store_id: fetched_order_data['data']['loja']['id'],
-          date: fetched_order_data['data']['data'],
-          alteration_date: alteration_date
-        )
-      end
+    orders_attributes = []
+    BlingOrderItem.where(bling_order_id: [query_bling_order_ids])
+                  .update_all(situation_id: @status, alteration_date: @alteration_date)
+
+    orders.each do |order|
+      next if query_bling_order_ids.include?(order['id'])
+
+      orders_attributes << {
+        bling_order_id: order['id'],
+        situation_id: order['situacao']['id'],
+        store_id: order['loja']['id'],
+        date: order['data'],
+        alteration_date:
+      }
     end
+
+    BlingOrderItem.create(orders_attributes)
   end
 end
