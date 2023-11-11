43916aa5dcc6f27a10f0eb66a17fbdf316ee3c15
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-11-11 10:59:49 -0300

    feat canceled_order_items_job.rb: task #325 Add canceled job.

--- app/jobs/canceled_order_items_job.rb
+++ app/jobs/canceled_order_items_job.rb
@@ -13,6 +13,7 @@ class CanceledOrderItemsJob < BlingOrderItemCreatorBaseJob
   attr_accessor :account_id
 
   def perform(account_id)
+    @status = STATUS
     @account_id = account_id
     begin
       orders = Services::Bling::Order.call(order_command: 'find_orders', tenant: account_id,
@@ -23,21 +24,5 @@ class CanceledOrderItemsJob < BlingOrderItemCreatorBaseJob
     rescue StandardError => e
       Rails.logger.error(e.message)
     end
-  end
-
-  def create_orders(orders)
-    orders_attributes = []
-
-    orders.each do |order|
-      orders_attributes << {
-        bling_order_id: order['id'],
-        situation_id: order['situacao']['id'],
-        store_id: order['loja']['id'],
-        date: order['data'],
-        alteration_date:
-      }
-    end
-
-    BlingOrderItem.upsert_all(orders_attributes, unique_by: :bling_order_id)
   end
 end
