2824853ad2667e71573a9e6fcc50821e3bef87f6
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-11-16 16:34:49 -0300

    fix canceled_bling_order_item_job_spec.rb, canceled_bling_order_items_job.rb: task #391 rename file and rename class

--- app/jobs/canceled_bling_order_items_job.rb
+++ app/jobs/canceled_bling_order_items_job.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
-# CanceledOrderItemsJob perform against created orders set previously
+# CanceledBlingOrderItemsJob perform against created orders set previously
 # with another status.
 # For this reason, we should call this job as an updater job.
 # We keep the create block as it is since we can not prove bling
 # erp first create a e.g. checked status then
 # update its own data to the canceled one.
-class CanceledOrderItemsJob < BlingOrderItemCreatorBaseJob
+class CanceledBlingOrderItemsJob < BlingOrderItemCreatorBaseJob
   queue_as :default
   STATUS = BlingOrderItem::Status::CANCELED.freeze
 
