48f1a560157afbef7ec90712ddfa0095220d1e2a
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-11-20 18:14:30 -0300

    test pending_order_items_job_spec.rb: task #396 add max page and date range options to the job.

--- spec/jobs/pending_order_items_job_spec.rb
+++ spec/jobs/pending_order_items_job_spec.rb
@@ -7,8 +7,31 @@ RSpec.describe PendingOrderItemsJob, type: :job do
 
   describe '#perform_now' do
     before do
-      BlingOrderItem.destroy_all
       FactoryBot.create(:bling_datum, account_id: user.account.id, expires_at: Time.now + 2.day)
+    end
+
+    context 'when max page option is given' do
+      let!(:options) { { max_pages: 1 } }
+
+      it 'counts by 100 bling order items' do
+        VCR.use_cassette('all_pending_order_items', erb: true) do
+          expect do
+            subject.perform(user.account.id, options)
+          end.to change(BlingOrderItem, :count).by(100)
+        end
+      end
+    end
+
+    context 'when date range option is given' do
+      let!(:options) { { dataInicial: '2023-11-1', dataFinal: '2023-11-1' } }
+
+      it 'counts by 100 bling order items' do
+        VCR.use_cassette('all_pending_order_items_date_range', erb: true) do
+          expect do
+            subject.perform(user.account.id, options)
+          end.to change(BlingOrderItem, :count).by(2)
+        end
+      end
     end
 
     context 'when there is no pending orders' do
