31c3758615ae96af6497c6244b4a2138d5c450f5
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-11-20 20:27:08 -0300

    fix order_spec.rb: task #396 record once.

--- spec/models/services/bling/order_spec.rb
+++ spec/models/services/bling/order_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe Services::Bling::Order, type: :services do
       let(:options) { { max_pages: 1 } }
 
       it 'counts 100 data' do
-        VCR.use_cassette('bling_order_items_max_pages', erb: true, record: :all) do
+        VCR.use_cassette('bling_order_items_max_pages', erb: true) do
           result = described_class.call(order_command: order_command, tenant: 1, situation: situation, options: options)
           expect(result['data'].count).to eq(100)
         end
