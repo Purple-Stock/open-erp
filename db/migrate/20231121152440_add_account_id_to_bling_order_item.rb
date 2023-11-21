8898ce4dd5f0b917542f11dbdb1baca211a19613
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-11-21 14:56:18 -0300

    fix migration: task #security foreign key false

--- db/migrate/20231121152440_add_account_id_to_bling_order_item.rb
+++ db/migrate/20231121152440_add_account_id_to_bling_order_item.rb
@@ -1,5 +1,5 @@
 class AddAccountIdToBlingOrderItem < ActiveRecord::Migration[7.0]
   def change
-    add_reference :bling_order_items, :account, foreign_key: true
+    add_reference :bling_order_items, :account, foreign_key: false
   end
 end
