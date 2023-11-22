29cbc87cb500821d341b0ad33f840f9ea73cee9a
puppe1990 <matheus.puppe90@hotmail.com>
2023-11-22 15:29:42 -0300

    add tenant

--- app/controllers/bling_order_items_controller.rb
+++ app/controllers/bling_order_items_controller.rb
@@ -4,6 +4,7 @@ class BlingOrderItemsController < ApplicationController
                            .select('bling_order_items.*, shein_orders.data ->> \'Pacote do comerciante\' as pacote_do_comerciante, shein_orders.data ->> \'Número do pedido\' as numero_do_pedido, shein_orders.data ->> \'Status do produto\' as status_do_produto')
                            .joins('LEFT JOIN shein_orders ON bling_order_items.marketplace_code_id = shein_orders.data->>\'Número do pedido\'')
                            .where(store_id: "204219105")
+                           .where(account_id: current_user.account.id)
                            .order(:situation_id)
   end
 end
