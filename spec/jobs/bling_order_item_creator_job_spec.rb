c5a719ddf05e2aa2c7a74dc13bf5536d349ed9a0
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-11-22 13:01:11 -0300

    chore jobs/*.rb: task #security update counter and order ids.

--- spec/jobs/bling_order_item_creator_job_spec.rb
+++ spec/jobs/bling_order_item_creator_job_spec.rb
@@ -12,11 +12,11 @@ RSpec.describe BlingOrderItemCreatorJob, type: :job do
       allow(subject).to receive(:list_status_situation).and_return([15])
     end
 
-    it 'counts by 84 bling order items' do
+    it 'counts by 43 bling order items' do
       VCR.use_cassette('all_situations_bling_order_items', erb: true) do
         expect do
           subject.perform(user.account.id)
-        end.to change(BlingOrderItem, :count).by(58)
+        end.to change(BlingOrderItem, :count).by(43)
       end
     end
   end
