dd9d9ae93bef419603ff81112b2b6dc537d4070a
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-11-22 12:25:30 -0300

    chore in_progress_order_items_job_spec.rb: task #security update counter.

--- spec/jobs/in_progress_order_items_job_spec.rb
+++ spec/jobs/in_progress_order_items_job_spec.rb
@@ -7,16 +7,7 @@ RSpec.describe InProgressOrderItemsJob, type: :job do
 
   describe '#perform_now' do
     before do
-      BlingOrderItem.destroy_all
       FactoryBot.create(:bling_datum, account_id: user.account.id, expires_at: Time.now + 2.day)
-    end
-
-    xit 'counts by 96 bling order items' do
-      VCR.use_cassette('all_in_progress_order_items', erb: true) do
-        expect do
-          subject.perform(user.account.id)
-        end.to change(BlingOrderItem, :count).by(96)
-      end
     end
 
     context 'when there is no in progress orders' do
@@ -24,28 +15,28 @@ RSpec.describe InProgressOrderItemsJob, type: :job do
         VCR.use_cassette('all_in_progress_order_items', erb: true) do
           expect do
             subject.perform(user.account.id)
-          end.to change(BlingOrderItem, :count).by(87)
+          end.to change(BlingOrderItem, :count).by(79)
         end
       end
     end
 
     context 'when there are in progress orders' do
       before do
-        FactoryBot.create(:bling_order_item, bling_order_id: '18971174289')
+        FactoryBot.create(:bling_order_item, bling_order_id: '19178034913')
       end
 
       it 'counts by 86 bling order items' do
         VCR.use_cassette('all_in_progress_order_items', erb: true) do
           expect do
             subject.perform(user.account.id)
-          end.to change(BlingOrderItem, :count).by(86) # one already created.
+          end.to change(BlingOrderItem, :count).by(78) # one already created.
         end
       end
 
       it 'has status in progress' do
         VCR.use_cassette('all_in_progress_order_items', erb: true) do
           subject.perform(user.account.id)
-          expect(BlingOrderItem.find_by(bling_order_id: '18971174289').situation_id.to_i)
+          expect(BlingOrderItem.find_by(bling_order_id: '19178034913').situation_id.to_i)
             .to eq(BlingOrderItem::Status::IN_PROGRESS)
         end
       end
