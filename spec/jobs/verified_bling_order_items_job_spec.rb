f7c90b48c7622fdc2a7a7773f83025521dcd5fa3
paulo-felipe <alparakleto@gmail.com>
2023-11-24 16:06:39 -0300

    fix printed_order_items_job_spec.rb: task security update order id and counter.

--- spec/jobs/verified_bling_order_items_job_spec.rb
+++ spec/jobs/verified_bling_order_items_job_spec.rb
@@ -12,11 +12,11 @@ RSpec.describe VerifiedBlingOrderItemsJob, type: :job do
     end
 
     context 'when there is no verified orders' do
-      it 'counts by 3027 bling order items' do
+      it 'counts by 3255 bling order items' do
         VCR.use_cassette('all_verified_order_items', erb: true) do
           expect do
             subject.perform(user.account.id)
-          end.to change(BlingOrderItem, :count).by(3027)
+          end.to change(BlingOrderItem, :count).by(3255)
         end
       end
     end
@@ -31,7 +31,7 @@ RSpec.describe VerifiedBlingOrderItemsJob, type: :job do
         VCR.use_cassette('all_verified_order_items', erb: true) do
           expect do
             subject.perform(user.account.id)
-          end.to change(BlingOrderItem, :count).by(3026) # one already created.
+          end.to change(BlingOrderItem, :count).by(3254) # one already created.
         end
       end
 
