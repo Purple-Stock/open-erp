b43b17fe360a946b2d623a8c1a1e2c4ee21b03de
paulo-felipe <alparakleto@gmail.com>
2023-11-24 15:51:34 -0300

    fix all_in_progress_order_items.rb: task security update order id and counter.

--- spec/jobs/in_progress_order_items_job_spec.rb
+++ spec/jobs/in_progress_order_items_job_spec.rb
@@ -11,32 +11,32 @@ RSpec.describe InProgressOrderItemsJob, type: :job do
     end
 
     context 'when there is no in progress orders' do
-      it 'counts by 43 bling order items' do
+      it 'counts by 100 bling order items' do
         VCR.use_cassette('all_in_progress_order_items', erb: true) do
           expect do
             subject.perform(user.account.id)
-          end.to change(BlingOrderItem, :count).by(43)
+          end.to change(BlingOrderItem, :count).by(100)
         end
       end
     end
 
     context 'when there are in progress orders' do
       before do
-        FactoryBot.create(:bling_order_item, bling_order_id: '19178124416')
+        FactoryBot.create(:bling_order_item, bling_order_id: '19195344030')
       end
 
-      it 'counts by 86 bling order items' do
+      it 'counts by 99 bling order items' do
         VCR.use_cassette('all_in_progress_order_items', erb: true) do
           expect do
             subject.perform(user.account.id)
-          end.to change(BlingOrderItem, :count).by(42) # one already created.
+          end.to change(BlingOrderItem, :count).by(99) # one already created.
         end
       end
 
       it 'has status in progress' do
         VCR.use_cassette('all_in_progress_order_items', erb: true) do
           subject.perform(user.account.id)
-          expect(BlingOrderItem.find_by(bling_order_id: '19178124416').situation_id.to_i)
+          expect(BlingOrderItem.find_by(bling_order_id: '19195344030').situation_id.to_i)
             .to eq(BlingOrderItem::Status::IN_PROGRESS)
         end
       end
