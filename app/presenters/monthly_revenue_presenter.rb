2598006e315111c1fa421ab621f404f1de1518bf
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-11-28 10:46:39 -0300

    refactor monthly_revenue_presenter.rb: #404 returns shein and shopee dataset structure.

--- app/presenters/monthly_revenue_presenter.rb
+++ app/presenters/monthly_revenue_presenter.rb
@@ -11,24 +11,21 @@ class MonthlyRevenuePresenter
   end
 
   def presentable
-    shein_values = []
-    shopee_values = []
+    [dataset_structure('shein'), dataset_structure('shopee')]
+  end
+
+  def dataset_structure(store_name)
+    values = []
     12.times do
-      shein_values << 0.0
-      shopee_values << 0.0
+      values << 0.0
     end
 
-    shein = { label: 'Shein', data: shein_values }
-    bling_order_item_collection.shein.group_by { |record| record.date.to_date.month }.map do |month, records|
-      shein[:data][month - 1] = records.sum(&:value).to_f
+    store_dataset = { label: store_name.capitalize, data: values }
+
+    bling_order_item_collection.send(store_name.to_sym).group_by { |record| record.date.to_date.month }.map do |month, records|
+      store_dataset[:data][month - 1] = records.sum(&:value).to_f
     end
 
-    shopee = { label: 'Shopee', data: shopee_values }
-
-    bling_order_item_collection.shopee.group_by { |record| record.date.to_date.month }.map do |month, records|
-      shopee[:data][month - 1] = records.sum(&:value).to_f
-    end
-
-    [shein, shopee]
+    store_dataset
   end
 end
