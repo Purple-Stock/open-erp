bc1909e6d72cd4dc424208de2c9965bfa4acbd78
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-11-28 16:29:27 -0300

    test monthly_revenue_presenter_spec.rb: #404 version without the filter date range. Show completely the datasets for chart.js.

--- spec/presenters/monthly_revenue_presenter_spec.rb
+++ spec/presenters/monthly_revenue_presenter_spec.rb
@@ -3,6 +3,33 @@
 require 'rails_helper'
 
 RSpec.describe MonthlyRevenuePresenter do
+  let(:november_shein_values) do
+    { label: 'Shein',
+      data: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 8.0, 0.0] }
+  end
+  let(:august_shopee_values) do
+    { label: 'Shopee',
+      data: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 6.0, 0.0, 0.0] }
+  end
+  let(:zero_simple_7_values) do
+    { label: 'Simple 7',
+      data: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0] }
+  end
+  let(:zero_mercado_livre_values) do
+    { label: 'Mercado Livre',
+      data: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0] }
+  end
+  let(:zeros_datasets) do
+    [{ label: 'Shein',
+       data: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0] },
+     { label: 'Shopee',
+       data: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0] },
+     { label: 'Simple 7',
+       data: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0] },
+     { label: 'Mercado Livre',
+       data: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0] }]
+  end
+
   describe '#initialize' do
     it 'raises argument error' do
       expect { described_class.new }.to raise_exception(ArgumentError)
@@ -12,23 +39,8 @@ RSpec.describe MonthlyRevenuePresenter do
   describe '#presentable' do
     context 'when there is not collection' do
       it 'is empty' do
-        empty_bling_order_item_collection = BlingOrderItem.all
-        expect(described_class.new(empty_bling_order_item_collection).presentable).to be_empty
-      end
-    end
-
-    context 'when there is data in the collection' do
-      before do
-        allow(Date).to receive(:today).and_return Date.new(2023, 11, 6)
-        FactoryBot.create_list(:bling_order_item, 4, store_id: '204219105')
-        FactoryBot.create_list(:bling_order_item, 3, store_id: '204219105', date: Date.new(2023, 10, 3))
-      end
-
-      it 'is an array of hash' do
-        result = [{ label: 'Shein',
-                    data: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 6.0, 8.0, 0.0] }]
-        bling_order_item_collection = BlingOrderItem.all
-        expect(described_class.new(bling_order_item_collection).presentable).to eq(result)
+        blank_collection = BlingOrderItem.all
+        expect(described_class.new(blank_collection).presentable).to eq(zeros_datasets)
       end
     end
 
@@ -40,10 +52,8 @@ RSpec.describe MonthlyRevenuePresenter do
       end
 
       it 'is an array of hash' do
-        result = [{ label: 'Shein',
-                    data: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0] }]
         bling_order_item_collection = BlingOrderItem.all
-        expect(described_class.new(bling_order_item_collection).presentable).to eq(result)
+        expect(described_class.new(bling_order_item_collection).presentable).to eq(zeros_datasets)
       end
     end
 
@@ -55,11 +65,8 @@ RSpec.describe MonthlyRevenuePresenter do
       end
 
       it 'is an array of hash' do
-        result = [{ label: 'Shein',
-                    data: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 8.0, 0.0] },
-                  label: 'Shopee',
-                  data: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 6.0, 0.0, 0.0]]
-        expect(described_class.new(BlingOrderItem.all).presentable).to eq(result)
+        datasets = [november_shein_values, august_shopee_values, zero_simple_7_values, zero_mercado_livre_values]
+        expect(described_class.new(BlingOrderItem.all).presentable).to eq(datasets)
       end
     end
   end
