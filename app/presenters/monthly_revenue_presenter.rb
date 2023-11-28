16d5d02c8ccd35d59514219dc481c2d510134423
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-11-28 10:29:03 -0300

    feat monthly_revenue_presenter.rb: #404 returns shein and shopee dataset structure.

--- app/presenters/monthly_revenue_presenter.rb
+++ app/presenters/monthly_revenue_presenter.rb
@@ -11,16 +11,24 @@ class MonthlyRevenuePresenter
   end
 
   def presentable
-    values = []
+    shein_values = []
+    shopee_values = []
     12.times do
-      values << 0.0
+      shein_values << 0.0
+      shopee_values << 0.0
     end
 
-    shein = { label: 'Shein', data: values }
+    shein = { label: 'Shein', data: shein_values }
     bling_order_item_collection.shein.group_by { |record| record.date.to_date.month }.map do |month, records|
       shein[:data][month - 1] = records.sum(&:value).to_f
     end
 
-    [shein]
+    shopee = { label: 'Shopee', data: shopee_values }
+
+    bling_order_item_collection.shopee.group_by { |record| record.date.to_date.month }.map do |month, records|
+      shopee[:data][month - 1] = records.sum(&:value).to_f
+    end
+
+    [shein, shopee]
   end
 end
