2fb994255a11ea9dc9b78d286d19a25451a5ecb0
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-11-29 20:03:57 -0300

    feat daily_revenue_presenter.rb: #404 return correct datasets for scenarios without filter date range. It still needs when date filter range is provided.

--- app/presenters/daily_revenue_presenter.rb
+++ app/presenters/daily_revenue_presenter.rb
@@ -3,15 +3,45 @@
 # DailyRevenuePresenter returns the hash the Chart js needs.
 # From the Char.js doc, https://www.chartjs.org/docs/latest/general/data-structures.html#parsing
 class DailyRevenuePresenter
-  attr_accessor :bling_order_item_collection, :filter
+  attr_accessor :bling_order_item_collection, :filter, :initial_date, :final_date
 
-  def initialize(bling_order_item_collection, filter = nil)
+  def initialize(bling_order_item_collection, date_filter = nil)
     @bling_order_item_collection = bling_order_item_collection
-    @filter = filter || Date.today
+    @filter = date_filter || { initial_date: Date.today, final_date: Date.today }
+    @initial_date = filter[:initial_date]
+    @final_date = filter[:final_date]
   end
 
   def presentable
-    zeros_datasets = [{ x: filter.strftime('%d/%m/%Y'), shein: 0.0, shopee: 0.0, simple_7: 0.0, mercado_livre: 0.0 }]
+    zeros_datasets = [{ x: initial_date.strftime('%d/%m/%Y'), shein: 0.0, shopee: 0.0, simple_7: 0.0,
+                        mercado_livre: 0.0 }]
     return zeros_datasets if bling_order_item_collection.blank?
+
+    datasets = []
+
+    record.group_by(&:date).map do |date, record|
+      axis_date = date.strftime('%d/%m/%Y')
+      shein_value = store_value_sum(record, '204219105')
+      shopee_value = store_value_sum(record,'203737982')
+      simple_7_value = store_value_sum(record, '203467890')
+      mercado_livre_value = store_value_sum(record, '204061683')
+      datasets << { x: axis_date,
+                    shein: shein_value,
+                    shopee: shopee_value,
+                    simple_7: simple_7_value,
+                    mercado_livre: mercado_livre_value }
+    end
+
+    datasets
+  end
+
+  private
+
+  def store_value_sum(record, store_id)
+    record.select { |object| object.store_id.eql?(store_id) }.sum(&:value).to_d
+  end
+
+  def record
+    bling_order_item_collection.date_range(initial_date, final_date)
   end
 end
