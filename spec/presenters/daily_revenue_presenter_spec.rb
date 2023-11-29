703af13cf6aab84fb4d8462452604d0f75a90900
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-11-29 11:02:25 -0300

    test daily_revenue_presenter_spec.rb: #404 Initial basic tests when filter by yesterday and today; when there is blank collection when the collection is present expect the datasets chart.js needs.

---
+++ spec/presenters/daily_revenue_presenter_spec.rb
@@ -0,0 +1,75 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe DailyRevenuePresenter do
+  describe '#initialize' do
+    it 'raises argument error' do
+      expect { described_class.new }.to raise_exception(ArgumentError)
+    end
+  end
+
+  describe '#presentable' do
+    context 'when there is not collection' do
+      let(:datasets) do
+        [{ x: '6/11/2023', shein: 0, shopee: 0, simple_7: 0, mercado_livre: 0 }]
+      end
+
+      it 'is empty' do
+        blank_collection = BlingOrderItem.all
+        expect(described_class.new(blank_collection).presentable).to eq(datasets)
+      end
+    end
+
+    context 'when there is data in the collection with nil in value' do
+      let(:datasets) do
+        [{ x: '6/11/2023', shein: 0, shopee: 0, simple_7: 0, mercado_livre: 0 }]
+      end
+
+      before do
+        allow(Date).to receive(:today).and_return Date.new(2023, 11, 6)
+        FactoryBot.create_list(:bling_order_item, 4, store_id: '204219105', value: nil)
+        FactoryBot.create_list(:bling_order_item, 3, store_id: '204219105', value: nil, date: Date.new(2023, 10, 3))
+      end
+
+      it 'is an array of hash' do
+        bling_order_item_collection = BlingOrderItem.all
+        expect(described_class.new(bling_order_item_collection).presentable).to eq(datasets)
+      end
+    end
+
+    context 'when there is data in the collection for both Shein and Shopee' do
+      let(:datasets) do
+        [{ x: '6/11/2023', shein: 8.0, shopee: 6.0, simple_7: 0, mercado_livre: 0 }]
+      end
+
+      before do
+        allow(Date).to receive(:today).and_return Date.new(2023, 11, 6)
+        FactoryBot.create_list(:bling_order_item, 4, store_id: '204219105')
+        FactoryBot.create_list(:bling_order_item, 3, store_id: '203737982')
+      end
+
+      it 'is an array of hash' do
+        expect(described_class.new(BlingOrderItem.all).presentable).to eq(datasets)
+      end
+    end
+
+    context 'when filter by today and yesterday' do
+      let(:filter) { { initial_date: '2023-11-5', final_date: '2023-11-6' } }
+      let(:datasets) do
+        [{ x: '5/11/2023', shein: 0.0, shopee: 0.0, simple_7: 0, mercado_livre: 0 },
+         { x: '6/11/2023', shein: 8.0, shopee: 6.0, simple_7: 0, mercado_livre: 0 }]
+      end
+
+      before do
+        allow(Date).to receive(:today).and_return Date.new(2023, 11, 6)
+        FactoryBot.create_list(:bling_order_item, 4, store_id: '204219105')
+        FactoryBot.create_list(:bling_order_item, 3, store_id: '203737982')
+      end
+
+      it 'has two axis x with today and yesterday value' do
+        expect(described_class.new(BlingOrderItem.all, filter).presentable).to eq(datasets)
+      end
+    end
+  end
+end
