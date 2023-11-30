8dff1f434c838479c05f5a82903abad68adef02b
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-11-30 17:50:08 -0300

    refactor bling_order_item_histories_spec.rb: #404 remove update chart ajax, the page will render with date range entirely on this current version. I have to figure out how to use turbo frame with chart js.

--- spec/requests/bling_order_item_histories_spec.rb
+++ spec/requests/bling_order_item_histories_spec.rb
@@ -4,13 +4,32 @@ require 'rails_helper'
 
 RSpec.describe 'BlingOrderItemHistories', type: :request do
   let(:user) { FactoryBot.create(:user) }
+  let(:params) do
+    { bling_order: { initial_date: (Date.today - 1.day).strftime,
+                     final_date: Date.today.strftime } }
+  end
 
   before { sign_in user }
 
   describe 'GET /index' do
-    it 'returns http success' do
-      get bling_order_item_histories_path
-      expect(response).to have_http_status(:success)
+    before do
+      allow(Date).to receive(:today).and_return Date.new(2023, 11, 9)
+      FactoryBot.create_list(:bling_order_item, 2, store_id: '204219105',
+                             account_id: user.account.id)
+    end
+
+    context 'when not filtering by date range' do
+      it 'returns http success' do
+        get bling_order_item_histories_path
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context 'when filtering by date range' do
+      it 'returns http success' do
+        get bling_order_item_histories_path(params)
+        expect(response).to have_http_status(:success)
+      end
     end
   end
 
@@ -34,64 +53,6 @@ RSpec.describe 'BlingOrderItemHistories', type: :request do
         get day_quantities_bling_order_item_histories_path
 
         expect(response).to have_http_status(:success)
-      end
-    end
-  end
-
-  describe 'GET /daily_revenue' do
-    context 'when there is not data in collection' do
-      it 'returns http success' do
-        get daily_revenue_bling_order_item_histories_path
-
-        expect(response).to have_http_status(:success)
-      end
-    end
-
-    context 'when there is data in collection filtered by initial and final date' do
-      let(:params) { { bling_order: { initial_date: (Date.today - 1.day).strftime, final_date: Date.today.strftime } } }
-      let(:date) { Date.today }
-      let(:datasets) do
-        [{ 'mercado_livre' => 0.0,
-           'shein' => 4.0,
-           'shopee' => 0.0,
-           'simple_7' => 0.0,
-           'x' => date.strftime('%d/%m/%Y'),
-           'total' => 4.0 }]
-      end
-
-      let(:filtered_datasets) do
-        [{ 'mercado_livre' => 0.0, 'shein' => 0.0,
-           'shopee' => 0.0,
-           'simple_7' => 0.0,
-           'x' => (date - 1.day).strftime('%d/%m/%Y'),
-           'total' => 0.0 },
-         { 'mercado_livre' => 0.0, 'shein' => 4.0,
-           'shopee' => 0.0,
-           'simple_7' => 0.0,
-           'x' => date.strftime('%d/%m/%Y'),
-           'total' => 4.0 }]
-      end
-
-      before do
-        allow(Date).to receive(:today).and_return Date.new(2023, 11, 9)
-        FactoryBot.create_list(:bling_order_item, 2, store_id: '204219105',
-                                                     account_id: user.account.id)
-      end
-
-      it 'matches array of datasets' do
-        get daily_revenue_bling_order_item_histories_path
-
-        result = JSON.parse(response.body)
-
-        expect(result).to match_array(datasets)
-      end
-
-      it 'matches array of filtered datasets' do
-        get daily_revenue_bling_order_item_histories_path(params)
-
-        result = JSON.parse(response.body)
-
-        expect(result).to match_array(filtered_datasets)
       end
     end
   end
