aec3f7d1bb84d337073b97ccc95086e2b89eaaff
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-11-29 21:53:35 -0300

    feat daily_revenue_presenter.rb: #404 return correct datasets for scenarios with filter date range.

--- app/presenters/daily_revenue_presenter.rb
+++ app/presenters/daily_revenue_presenter.rb
@@ -13,32 +13,39 @@ class DailyRevenuePresenter
   end
 
   def presentable
-    zeros_datasets = [{ x: initial_date.strftime('%d/%m/%Y'), shein: 0.0, shopee: 0.0, simple_7: 0.0,
+    zeros_datasets = [{ x: initial_date.to_date.strftime('%d/%m/%Y'), shein: 0.0, shopee: 0.0, simple_7: 0.0,
                         mercado_livre: 0.0 }]
     return zeros_datasets if bling_order_item_collection.blank?
 
     datasets = []
+    date_range = initial_date..final_date
+    date_range.each do |_date|
+      datasets << { x: initial_date.to_date.strftime('%d/%m/%Y'), shein: 0.0, shopee: 0.0, simple_7: 0.0,
+                    mercado_livre: 0.0 }
+    end
 
     record.group_by(&:date).map do |date, record|
       axis_date = date.strftime('%d/%m/%Y')
       shein_value = store_value_sum(record, '204219105')
-      shopee_value = store_value_sum(record,'203737982')
+      shopee_value = store_value_sum(record, '203737982')
       simple_7_value = store_value_sum(record, '203467890')
       mercado_livre_value = store_value_sum(record, '204061683')
-      datasets << { x: axis_date,
-                    shein: shein_value,
-                    shopee: shopee_value,
-                    simple_7: simple_7_value,
-                    mercado_livre: mercado_livre_value }
+      datasets_date_index = datasets.find_index({ x: initial_date.to_date.strftime('%d/%m/%Y'), shein: 0.0, shopee: 0.0, simple_7: 0.0,
+                                                  mercado_livre: 0.0 })
+      datasets[datasets_date_index] = { x: axis_date,
+                                        shein: shein_value,
+                                        shopee: shopee_value,
+                                        simple_7: simple_7_value,
+                                        mercado_livre: mercado_livre_value }
     end
 
-    datasets
+    datasets.sort_by! { |dataset| dataset[:x] }
   end
 
   private
 
   def store_value_sum(record, store_id)
-    record.select { |object| object.store_id.eql?(store_id) }.sum(&:value).to_d
+    record.select { |object| object.store_id.eql?(store_id) }.sum(&:value).to_f
   end
 
   def record
