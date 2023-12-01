b04312da4b279fc4ce40078c8cebb7e56a2934be
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-12-01 19:47:59 -0300

    fix daily_revenue_report.rb: #434 change datasets expectations. When the filter is used group stores by values In other words, I wanna know the revenue for this store considering a date range search.

--- app/reports/daily_revenue_report.rb
+++ app/reports/daily_revenue_report.rb
@@ -3,54 +3,28 @@
 # DailyRevenueReport returns the hash the Chart js needs.
 # From the Char.js doc, https://www.chartjs.org/docs/latest/general/data-structures.html#parsing
 class DailyRevenueReport
-  attr_accessor :bling_order_item_collection, :filter, :initial_date, :final_date
+  attr_accessor :bling_order_item_collection, :filter, :initial_date, :final_date, :record, :axi_x
 
   def initialize(bling_order_item_collection, date_filter = nil)
     @bling_order_item_collection = bling_order_item_collection
     @filter = date_filter || { initial_date: Date.today, final_date: Date.today }
     @initial_date = filter[:initial_date]
     @final_date = filter[:final_date]
+    @axi_x = filter.values.uniq.map { |date| date.to_date.strftime('%d/%m/%Y') }.join(' - ')
+    @record ||= bling_order_item_collection.date_range(initial_date, final_date)
   end
 
   def presentable
-    zeros_datasets = [{ x: initial_date.to_date.strftime('%d/%m/%Y'), shein: 0.0, shopee: 0.0, simple_7: 0.0,
-                        mercado_livre: 0.0, total: 0.0 }]
-    return zeros_datasets if bling_order_item_collection.blank?
-
-    datasets = []
-    date_range = initial_date..final_date
-    date_range.each do |_date|
-      datasets << { x: initial_date.to_date.strftime('%d/%m/%Y'), shein: 0.0, shopee: 0.0, simple_7: 0.0,
-                    mercado_livre: 0.0, total: 0.0 }
-    end
-
-    record.group_by(&:date).map do |date, record|
-      axis_date = date.strftime('%d/%m/%Y')
-      shein_value = store_value_sum(record, '204219105')
-      shopee_value = store_value_sum(record, '203737982')
-      simple_7_value = store_value_sum(record, '203467890')
-      mercado_livre_value = store_value_sum(record, '204061683')
-      total_value = shein_value + shopee_value + simple_7_value + mercado_livre_value
-      datasets_date_index = datasets.find_index({ x: initial_date.to_date.strftime('%d/%m/%Y'), shein: 0.0, shopee: 0.0, simple_7: 0.0,
-                                                  mercado_livre: 0.0, total: 0.0 })
-      datasets[datasets_date_index] = { x: axis_date,
-                                        shein: shein_value,
-                                        shopee: shopee_value,
-                                        simple_7: simple_7_value,
-                                        mercado_livre: mercado_livre_value,
-                                        total: total_value}
-    end
-
-    datasets.sort_by! { |dataset| dataset[:x] }
-  end
-
-  private
-
-  def store_value_sum(record, store_id)
-    record.select { |object| object.store_id.eql?(store_id) }.sum(&:value).to_f
-  end
-
-  def record
-    bling_order_item_collection.date_range(initial_date, final_date)
+    shein_value_sum = record.shein.sum(&:value).to_f
+    shopee_value_sum = record.shopee.sum(&:value).to_f
+    simple_7_value_sum = record.simple_7.sum(&:value).to_f
+    mercado_livre_value_sum = record.mercado_livre.sum(&:value).to_f
+    total = shein_value_sum + shopee_value_sum + simple_7_value_sum + mercado_livre_value_sum
+    [{ x: axi_x,
+      shein: shein_value_sum,
+      shopee: shopee_value_sum,
+      simple_7: simple_7_value_sum,
+      mercado_livre: mercado_livre_value_sum,
+       total: total}]
   end
 end
