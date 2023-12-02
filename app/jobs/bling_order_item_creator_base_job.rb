5d29f6d314a457e2176c5068ac7935959be64f26
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-12-02 12:34:58 -0300

    feat bling_order_item_creator_base_job.rb: #437 update order value

--- app/jobs/bling_order_item_creator_base_job.rb
+++ app/jobs/bling_order_item_creator_base_job.rb
@@ -20,26 +20,41 @@ class BlingOrderItemCreatorBaseJob < ApplicationJob
     query_bling_order_ids = BlingOrderItem.select(:bling_order_id).where(bling_order_id: order_ids)
                                           .map { |order| order.bling_order_id.to_i }
 
-    orders_attributes = []
-    BlingOrderItem.where(bling_order_id: [query_bling_order_ids])
-                  .update_all(situation_id: @status, alteration_date: @alteration_date, account_id:)
+    create_orders_attributes = []
+    update_orders_attributes = []
 
     orders.each do |order|
-      next if query_bling_order_ids.include?(order['id'])
-
-      orders_attributes << {
-        bling_order_id: order['id'],
-        situation_id: order['situacao']['id'],
-        store_id: order['loja']['id'],
-        date: order['data'],
-        alteration_date:,
-        marketplace_code_id: order['numeroLoja'],
-        bling_id: order['numero'],
-        account_id:,
-        value: order['total']
-      }
+      if query_bling_order_ids.include?(order['id'])
+        update_orders_attributes << {
+          bling_order_id: order['id'],
+          situation_id: order['situacao']['id'],
+          store_id: order['loja']['id'],
+          date: order['data'],
+          alteration_date:,
+          marketplace_code_id: order['numeroLoja'],
+          bling_id: order['numero'],
+          account_id:,
+          value: order['total']
+        }
+      else
+        create_orders_attributes << {
+          bling_order_id: order['id'],
+          situation_id: order['situacao']['id'],
+          store_id: order['loja']['id'],
+          date: order['data'],
+          alteration_date:,
+          marketplace_code_id: order['numeroLoja'],
+          bling_id: order['numero'],
+          account_id:,
+          value: order['total']
+        }
+      end
     end
 
-    BlingOrderItem.create(orders_attributes)
+    BlingOrderItem.create(create_orders_attributes)
+
+    update_orders_attributes.each do |update_attribute|
+      BlingOrderItem.find_by(bling_order_id: update_attribute[:bling_order_id]).update(update_attribute)
+    end
   end
 end
