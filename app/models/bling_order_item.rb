1f4084eb425116aecff3fd6b10ea1382d1751e79
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-12-05 14:32:44 -0300

    fix bling_order_item.rb: #444  consider errors in response.

--- app/models/bling_order_item.rb
+++ app/models/bling_order_item.rb
@@ -110,6 +110,8 @@ class BlingOrderItem < ApplicationRecord
 
     self_collection.each do |record|
       result = Services::Bling::FindOrder.call(id: record.bling_order_id, order_command: 'find_order', tenant: account_id)
+      next if result['error'].present?
+
       record.update({ value: result['data']['total'], situation_id: result['data']['situacao']['id'] })
     end
   end
