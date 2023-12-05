fc862407abec5fa37c77a31e20b02eb86d489d6b
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-12-05 18:06:26 -0300

    refactor bling_order_item.rb, bling_order_job_updater.rb: #444  async version.

--- app/models/bling_order_item.rb
+++ app/models/bling_order_item.rb
@@ -109,10 +109,7 @@ class BlingOrderItem < ApplicationRecord
     account_id = self_collection.first.account_id
 
     self_collection.each do |record|
-      result = Services::Bling::FindOrder.call(id: record.bling_order_id, order_command: 'find_order', tenant: account_id)
-      next if result['error'].present?
-
-      record.update({ value: result['data']['total'], situation_id: result['data']['situacao']['id'] })
+      BlingOrderJobUpdater.perform_later(record, account_id)
     end
   end
 
