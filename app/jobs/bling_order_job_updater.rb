82cc8afd80affa8ba549f66f963bc7cd70be6464
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-12-05 18:12:15 -0300

    fix bling_order_job_updater.rb: #444  async version just return.

--- app/jobs/bling_order_job_updater.rb
+++ app/jobs/bling_order_job_updater.rb
@@ -3,7 +3,7 @@ class BlingOrderJobUpdater < ApplicationJob
 
   def perform(record, account_id)
     result = Services::Bling::FindOrder.call(id: record.bling_order_id, order_command: 'find_order', tenant: account_id)
-    raise StandardError if result['error'].present?
+    return if result['error'].present?
 
     record.update({ value: result['data']['total'], situation_id: result['data']['situacao']['id'] })
   end
