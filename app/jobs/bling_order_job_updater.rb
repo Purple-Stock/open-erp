d356307fe3fab33e87f9b2455ee975cd76770e8c
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-12-06 14:12:04 -0300

    feat bling_order_job_updater.rb: #447  It retries on raising error.

--- app/jobs/bling_order_job_updater.rb
+++ app/jobs/bling_order_job_updater.rb
@@ -1,9 +1,10 @@
 class BlingOrderJobUpdater < ApplicationJob
   queue_as :default
+  retry_on StandardError, attempts: 5, wait: :exponentially_longer
 
   def perform(record, account_id)
     result = Services::Bling::FindOrder.call(id: record.bling_order_id, order_command: 'find_order', tenant: account_id)
-    return if result['error'].present?
+    raise StandardError if result['error'].present?
 
     record.update({ value: result['data']['total'], situation_id: result['data']['situacao']['id'] })
   end
