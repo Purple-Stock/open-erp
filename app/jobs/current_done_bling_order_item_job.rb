e5800fbfe251efc59f901aa5cae75ca168a8c6f0
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-12-06 19:11:08 -0300

    feat canceled_bling_order_items_job.rb, checked_bling_order_items_job.rb, current_done_bling_order_item_job.rb, in_progress_order_items_job.rb, printed_order_items_job.rb, verified_bling_order_items_job.rb: #447  all jobs must be retried on exception.  Also remove queue_as since it is inherited from parent.

--- app/jobs/current_done_bling_order_item_job.rb
+++ app/jobs/current_done_bling_order_item_job.rb
@@ -7,17 +7,13 @@ class CurrentDoneBlingOrderItemJob < BlingOrderItemCreatorBaseJob
     @alteration_date = Date.today.strftime('%Y-%m-%d')
     options = { dataAlteracaoInicial: alteration_date }
     @account_id = account_id
-    begin
-      DONE_STATUSES.each do |status|
-        @status = status
-        orders = Services::Bling::Order.call(order_command: 'find_orders', tenant: account_id,
-                                             situation: status, options:)
-        orders = orders['data']
+    DONE_STATUSES.each do |status|
+      @status = status
+      orders = Services::Bling::Order.call(order_command: 'find_orders', tenant: account_id,
+                                           situation: status, options:)
+      orders = orders['data']
 
-        create_orders(orders)
-      end
-    rescue StandardError => e
-      Rails.logger.error(e.message)
+      create_orders(orders)
     end
   end
 end
