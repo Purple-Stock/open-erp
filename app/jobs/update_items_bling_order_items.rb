df9706371abcb86bf191bddd5e48b94c91314442
puppe1990 <matheus.puppe90@hotmail.com>
2023-12-15 16:04:48 -0300

    create a filter for pending products

---
+++ app/jobs/update_items_bling_order_items.rb
@@ -0,0 +1,8 @@
+class UpdateItemsBlingOrderItem < ApplicationJob
+  queue_as :default
+
+  def perform(store_id, situation_id, current_tenant_id)
+    bling_order_ids = BlingOrderItem.where(store_id: store_id, situation_id: situation_id, items: nil).pluck(:bling_order_id)
+    Services::Bling::FindOrders.call(order_command: 'find_orders', tenant: current_tenant_id, ids: bling_order_ids) 
+  end
+end
