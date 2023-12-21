7f8f0974ebaa1cc98c9871077c1020e79af55fa9
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-12-21 13:47:43 -0300

    test factories/stocks.rb #7   definition

---
+++ spec/factories/stocks.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: stocks
+#
+#  id                    :bigint           not null, primary key
+#  total_balance         :integer
+#  total_virtual_balance :integer
+#  created_at            :datetime         not null
+#  updated_at            :datetime         not null
+#  bling_product_id      :bigint
+#  product_id            :integer
+#
+FactoryBot.define do
+  factory :stock do
+    product_id { 1 }
+    bling_product_id { 1 }
+    total_balance { 1 }
+    total_virtual_balance { 1 }
+  end
+end
