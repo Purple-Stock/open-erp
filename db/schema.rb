a62099dd0482b39cd128add523953d71ca0faf40
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-12-21 13:44:56 -0300

    chore schema.rb: #7   add Stock model table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_20_232520) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_21_140612) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -373,6 +373,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_20_232520) do
   create_table "simplo_products", force: :cascade do |t|
     t.string "name"
     t.string "sku"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "stocks", force: :cascade do |t|
+    t.integer "product_id"
+    t.bigint "bling_product_id"
+    t.integer "total_balance"
+    t.integer "total_virtual_balance"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
