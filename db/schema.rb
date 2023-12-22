a751a8d340037ada0a0481a20dcb9d168fe6882e
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-12-21 21:49:19 -0300

    chore schema.rb: #7 update schema with account id.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_21_140612) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_21_234141) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -384,6 +384,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_21_140612) do
     t.integer "total_virtual_balance"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "account_id"
   end
 
   create_table "stores", force: :cascade do |t|
