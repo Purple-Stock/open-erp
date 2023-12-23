4dbb66c6a68572a309557a3d8b67ac380a4a1fac
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-12-23 19:21:26 -0300

    feat stocks_controller.rb: task #7 keep filters in memory

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -7,6 +7,9 @@ class StocksController < ApplicationController
   protected
 
   def collection
+    @default_status_filter = params['status']
+    @default_situation_balance_filter = params['balance_situation']
+
     stocks = Stock.where(account_id: current_tenant).filter_by_status(params['status'])
                   .filter_by_total_balance_situation(params['balance_situation'])
     @pagy, @stocks = pagy(stocks)
