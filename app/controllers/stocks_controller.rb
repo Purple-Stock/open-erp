7e5590d6652294fd57c85cc96be18d18b6c3495d
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-12-23 12:59:35 -0300

    feat stocks_controller.rb: task #7 Add pagination

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
 class StocksController < ApplicationController
+  include Pagy::Backend
   inherit_resources
+
+  protected
+
+  def collection
+    stocks = Stock.where(account_id: current_tenant)
+    @pagy, @stocks = pagy(stocks)
+  end
 end
