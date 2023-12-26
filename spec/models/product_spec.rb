0ae1b45bf7fac767ab2a976406abb2c9fd4e9be0
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-12-26 17:10:31 -0300

    test product_spec.rb: task #7 must be greater or equal to zero.

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -48,6 +48,7 @@ RSpec.describe Product, type: :model do
 
   describe 'validations' do
     it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_numericality_of(:price).is_greater_than_or_equal_to(0) }
   end
 
   describe '#create' do
