f4b114fecc8b30d3c0b62c71cbc6fec6b8f377a8
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2023-12-27 12:52:13 -0300

    feat stocks_controller.rb: task #7 filter also by positive prices. The list of stocks is default to positive prices only

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -10,7 +10,9 @@ class StocksController < ApplicationController
     @default_status_filter = params['status']
     @default_situation_balance_filter = params['balance_situation']
 
-    stocks = Stock.where(account_id: current_tenant).filter_by_status(params['status'])
+    stocks = Stock.where(account_id: current_tenant)
+                  .only_positive_price(true)
+                  .filter_by_status(params['status'])
                   .filter_by_total_balance_situation(params['balance_situation'])
     @pagy, @stocks = pagy(stocks)
   end
