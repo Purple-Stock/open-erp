b45e31c1d5e11f72072545cbe6dac8f4476b2e56
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2024-01-10 12:49:37 -0300

    test forecasts/basic_stock_spec.rb: task #32 describe #count_sold

--- spec/lib/stock_forecasts/basic_stock_spec.rb
+++ spec/lib/stock_forecasts/basic_stock_spec.rb
@@ -4,19 +4,28 @@ require 'rails_helper'
 require 'forecasts/basic_stock'
 
 RSpec.describe ::Forecasts::BasicStock do
+  include_context 'when user account'
+  let(:sku) { 'VEST' }
+  let(:product) { FactoryBot.create(:product, sku: sku, account_id: user.account.id) }
+  let(:order) { FactoryBot.create(:bling_order_item, date: Date.new(2024, 1, 1), account: user.account) }
+  let(:stock) { FactoryBot.create(:stock, total_balance: 0, account_id: user.account.id, product: product) }
+
+  before do
+    allow(Date).to receive(:today).and_return Date.new(2024, 1, 9)
+    FactoryBot.create(:item, sku: sku, quantity: 2, bling_order_item: order, product_id: product.id, account: user.account)
+  end
+
+  describe '#count_sold' do
+    subject(:count_sold) { described_class.new(stock).count_sold }
+
+    it 'counts 2' do
+      expect(count_sold).to eq(2)
+    end
+  end
+
   describe '#calculate' do
     subject(:calculate) { described_class.new(stock).calculate }
 
-    include_context 'when user account'
-    let(:sku) { 'VEST' }
-    let(:product) { FactoryBot.create(:product, sku: sku, account_id: user.account.id) }
-    let(:order) { FactoryBot.create(:bling_order_item, date: Date.new(2024, 1, 1), account: user.account) }
-    let(:stock) { FactoryBot.create(:stock, total_balance: 0, account_id: user.account.id, product: product) }
-
-    before do
-      allow(Date).to receive(:today).and_return Date.new(2024, 1, 9)
-      FactoryBot.create(:item, sku: sku, quantity: 2, bling_order_item: order, product_id: product.id, account: user.account)
-    end
 
     context 'when there is no order related to stock' do
       before { order.destroy }
