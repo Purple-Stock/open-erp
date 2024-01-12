1396885f29647468399732cc2c9d0f213017fef9
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2024-01-12 18:06:11 -0300

    refactor bling_order_item.rb: task 68 update_yourself with batch version.

--- app/models/bling_order_item.rb
+++ app/models/bling_order_item.rb
@@ -119,8 +119,13 @@ class BlingOrderItem < ApplicationRecord
   def self.update_yourself(self_collection)
     account_id = self_collection.first.account_id
 
-    self_collection.each do |record|
-      BlingOrderJobUpdater.perform_later(record, account_id)
+    self_collection.find_in_batches(batch_size: 100) do |sub_collection|
+      GoodJob::Bulk.enqueue do
+        sub_collection.each do |record|
+          BlingOrderJobUpdater.perform_later(record, account_id)
+        end
+      end
+      sleep 10 if Rails.env.eql?('production') || Rails.env.eql?('staging')
     end
   end
 
