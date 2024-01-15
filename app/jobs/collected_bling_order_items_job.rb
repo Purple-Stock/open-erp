b765583366186cde22b4be780d6c37623b2e0d4a
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2024-01-15 15:39:09 -0300

    feat collected_bling_order_items_job.rb: task 75 customize its own collected creation with collected alteration date.

--- app/jobs/collected_bling_order_items_job.rb
+++ app/jobs/collected_bling_order_items_job.rb
@@ -21,14 +21,46 @@ class CollectedBlingOrderItemsJob < BlingOrderItemCreatorBaseJob
     @final_date = Date.today
     date_range = (@initial_date..@final_date)
     date_range.each do |alteration_date|
-      @alteration_date = alteration_date
+      @collected_alteration_date = alteration_date
       final_alteration_date = (alteration_date + 1.day).strftime
-      options = { dataAlteracaoInicial: @alteration_date.strftime, dataAlteracaoFinal: final_alteration_date }
+      options = { dataAlteracaoInicial: @collected_alteration_date.strftime, dataAlteracaoFinal: final_alteration_date }
       orders = Services::Bling::Order.call(order_command: 'find_orders', tenant: account_id,
                                            situation: STATUS, options: options)
       orders = orders['data']
 
       create_orders(orders)
     end
+  end
+
+  def create_orders(orders)
+    return if orders.blank?
+
+    order_ids = orders.map { |order| order['id'] }
+
+    query_bling_order_ids = BlingOrderItem.where(bling_order_id: order_ids)
+                                          .map { |order| order.bling_order_id.to_i }
+
+    orders_attributes = []
+    BlingOrderItem.where(bling_order_id: [query_bling_order_ids])
+                  .update_all(situation_id: @status, collected_alteration_date: @collected_alteration_date, account_id:)
+
+    orders.each do |order|
+      next if query_bling_order_ids.include?(order['id'])
+
+      orders_attributes << {
+        bling_order_id: order['id'],
+        situation_id: order['situacao']['id'],
+        store_id: order['loja']['id'],
+        date: order['data'],
+        collected_alteration_date: @collected_alteration_date,
+        alteration_date: @collected_alteration_date,
+        marketplace_code_id: order['numeroLoja'],
+        bling_id: order['numero'],
+        account_id:,
+        value: order['total']
+      }
+    end
+
+    BlingOrderItem.create(orders_attributes)
   end
 end
