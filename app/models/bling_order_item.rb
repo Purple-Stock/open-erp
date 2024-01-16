63eafa76f00b7acb57ae72e3970d5c1280126324
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2024-01-16 14:45:49 -0300

    feat bling_order_item.rb: task 79 collected! it sets collected alteration date equal to old alteration date only if situation id is collected.

--- app/models/bling_order_item.rb
+++ app/models/bling_order_item.rb
@@ -37,6 +37,8 @@ class BlingOrderItem < ApplicationRecord
   has_many :items, dependent: :destroy
   belongs_to :account, optional: true
 
+  before_update :set_collected_alteration_date, if: :collected?
+
   accepts_nested_attributes_for :items
 
   after_create :synchronize_items
@@ -150,5 +152,15 @@ class BlingOrderItem < ApplicationRecord
     GoodJob::Bulk.enqueue do
       collection.each(&:synchronize_items)
     end
+  end
+
+  private
+
+  def set_collected_alteration_date
+    self.collected_alteration_date = alteration_date
+  end
+
+  def collected?
+    situation_id.eql?(BlingOrderItem::Status::COLLECTED.to_s)
   end
 end
