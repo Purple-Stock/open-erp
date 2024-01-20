9f01202b99a35efd2c4084517f9853b620ff8815
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2024-01-20 12:26:32 -0300

    chore seeds.rb: task #95 use a expires_at date more coherent with the business rules.

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ Rails.logger.debug 'Module User'
 
 FactoryBot.create(:user, password: '123456', email: 'fashion.store@email.com')
 
-FactoryBot.create(:bling_datum, account_id: 1, expires_at: (Time.zone.local(13) + 3.days),
+FactoryBot.create(:bling_datum, account_id: 1, expires_at: (Time.zone.now + 3.days),
                                 access_token: ENV['ACCESS_TOKEN'], refresh_token: ENV['REFRESH_TOKEN'])
 
 Rails.logger.debug 'Categories'
