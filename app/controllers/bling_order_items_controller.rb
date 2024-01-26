7d1ae29bca62fb87c555d3e60522270b67883cdc
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2024-01-26 14:49:27 -0300

    feat bling_order_items_controller.rb: task #113 filter by store with default store all.

--- app/controllers/bling_order_items_controller.rb
+++ app/controllers/bling_order_items_controller.rb
@@ -8,10 +8,12 @@ class BlingOrderItemsController < ApplicationController
     @default_status_filter = params['status']
     @default_initial_date = params['initial_date'] || Date.today
     @default_final_date = params['final_date'] || Date.today
+    @default_store_filter = params['store_id'] || BlingOrderItemStore::ALL
 
     bling_order_items = BlingOrderItem.where(account_id: current_tenant)
                                       .by_status(@default_status_filter)
                                       .date_range(@default_initial_date, @default_final_date)
+                                      .by_store(@default_store_filter)
     @pagy, @bling_order_items = pagy(bling_order_items)
   end
 end
