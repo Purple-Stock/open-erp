4cc2c5c5bde4d282b0381478c8ccd0cc77d9b09f
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2024-02-01 17:17:20 -0300

    feat bling_order_item_decorator.rb: task #132 add situation_id and store_id humanized by delegation. add #date, #alteration_date, #created_at and #updated_at

--- app/decorators/bling_order_item_decorator.rb
+++ app/decorators/bling_order_item_decorator.rb
@@ -3,8 +3,35 @@
 # Decorator Pattern applied, check https://github.com/drapergem/draper
 class BlingOrderItemDecorator < Draper::Decorator
   include AbstractController::Translation
+  delegate :situation_id_humanize, :store_id_humanize
 
   def title
     "#{t('activerecord.attributes.bling_order_items.bling_order_id')} #{model.bling_order_id}"
+  end
+
+  def situation_id
+    situation_id_humanize
+  end
+
+  def store_id
+    store_id_humanize
+  end
+
+  def created_at
+    localize(model.created_at, format: :default)
+  end
+
+  def updated_at
+    localize(model.updated_at, format: :default)
+  end
+
+  def date
+    localize(model.date, format: :short)
+  end
+
+  def alteration_date
+    return if model.alteration_date.blank?
+
+    localize(model.alteration_date, format: :short)
   end
 end
