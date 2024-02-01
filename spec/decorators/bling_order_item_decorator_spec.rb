134e08b9e0a7b796f2536c2e3eb5a853a38c75cf
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2024-02-01 17:16:41 -0300

    test bling_order_item_decorator_spec.rb: task #132 describe situation_id and store_id humanized by delegation. describe #date, #alteration_date, #created_at and #updated_at

--- spec/decorators/bling_order_item_decorator_spec.rb
+++ spec/decorators/bling_order_item_decorator_spec.rb
@@ -13,4 +13,50 @@ RSpec.describe BlingOrderItemDecorator, type: :decorator do
       expect(bling_order_decorated.title).to eq(title)
     end
   end
+
+  describe '#situation_id' do
+    it 'is humanized' do
+      expect(bling_order_decorated.situation_id).to eq(t('enumerations.bling_order_item_status.checked'))
+    end
+  end
+
+  describe '#store_id' do
+    it 'is humanized' do
+      expect(bling_order_decorated.store_id).to eq('Shein')
+    end
+  end
+
+  describe '#created_at' do
+    it 'uses default locales' do
+      expect(bling_order_decorated.created_at).to eq(localize(bling_order.created_at, format: :default))
+    end
+  end
+
+  describe '#updated_at' do
+    it 'uses default locales' do
+      expect(bling_order_decorated.updated_at).to eq(localize(bling_order.updated_at, format: :default))
+    end
+  end
+
+  describe '#date' do
+    it 'uses default locales' do
+      expect(bling_order_decorated.date).to eq(localize(bling_order.date, format: :short))
+    end
+  end
+
+  describe '#alteration_date' do
+    context 'when there is alteration date' do
+      it 'uses short locales' do
+        expect(bling_order_decorated.alteration_date).to eq(localize(bling_order.alteration_date, format: :short))
+      end
+    end
+
+    context 'when alteration date is blank' do
+      before { bling_order.alteration_date = nil }
+
+      it 'shows warning about blank alteration date' do
+        expect(bling_order_decorated.alteration_date).to be_blank
+      end
+    end
+  end
 end
