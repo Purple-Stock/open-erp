095ddfb54e063995893b7984883fe2e1328d87be
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2024-02-05 16:20:17 -0300

    fix bling_order_item_decorator.rb: task #140 update decorator translation path

--- app/decorators/bling_order_item_decorator.rb
+++ app/decorators/bling_order_item_decorator.rb
@@ -7,7 +7,7 @@ class BlingOrderItemDecorator < Draper::Decorator
   delegate_all
 
   def title
-    "#{t('activerecord.attributes.bling_order_items.bling_order_id')} #{model.bling_order_id}"
+    "#{t('activerecord.attributes.bling_order_item.bling_order_id')} #{model.bling_order_id}"
   end
 
   def situation_id
