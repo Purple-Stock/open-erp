58938327b1c37a625544ccb4b76d36a36009fb9b
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2024-02-06 18:09:26 -0300

    feat bling_order_item_destroyer_job.rb: task #137 update order if not found at bling otherwise update to original situation id.

--- app/jobs/bling_order_item_destroyer_job.rb
+++ app/jobs/bling_order_item_destroyer_job.rb
@@ -1,8 +1,10 @@
 class BlingOrderItemDestroyerJob < ApplicationJob
   def perform(bling_order_id)
     order = BlingOrderItem.find_by(bling_order_id:)
-    return unless order.not_found_at_bling?
-
-    order.update(situation_id: BlingOrderItemStatus::DELETED_AT_BLING)
+    if order.not_found_at_bling?
+      order.update(situation_id: BlingOrderItemStatus::DELETED_AT_BLING)
+    else
+      order.update(situation_id: order.original_situation_id)
+    end
   end
 end
