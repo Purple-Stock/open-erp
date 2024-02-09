da9bbc9090b0466dee78698d75731dc808b90716
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2024-02-09 17:06:40 -0300

    test account_feature_spec.rb: task #104 describe is_enabled? when feature key is stock it is true; when it is bling integration, it is false.

--- spec/models/account_feature_spec.rb
+++ spec/models/account_feature_spec.rb
@@ -12,5 +12,22 @@
 require 'rails_helper'
 
 RSpec.describe AccountFeature, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#is_enabled?' do
+    let!(:feature) { FactoryBot.create(:feature, feature_key: FeatureKey::STOCK, name: 'Stockist') }
+    let!(:user) { FactoryBot.create(:user) }
+
+    context 'when feature key is STOCK' do
+      it 'is truthy' do
+        expect(user.account.account_features.first.is_enabled?).to eq(true)
+      end
+    end
+
+    context 'when feature key is BLING_INTEGRATION' do
+      before { feature.update(feature_key: FeatureKey::BLING_INTEGRATION) }
+
+      it 'is false' do
+        expect(user.account.account_features.first.is_enabled?).to eq(false)
+      end
+    end
+  end
 end
