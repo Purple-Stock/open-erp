342e0c6d1b9f18a4dd263d9fba985e9fb49ff4d3
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2024-02-09 13:04:50 -0300

    feat models/ feature.rb: task #104 add has many to many model table

---
+++ app/models/feature.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+#
+# Table name: features
+#
+#  id          :bigint           not null, primary key
+#  feature_key :integer          default(0), not null
+#  is_enabled  :boolean          default(FALSE), not null
+#  name        :string           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+class Feature < ApplicationRecord
+  has_many :account_features
+  has_many :accounts, through: :account_features
+end
