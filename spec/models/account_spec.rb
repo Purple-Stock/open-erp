0989313eb9ae782fb1a61c5fe2fba2adb1123bd9
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2024-02-09 12:59:36 -0300

    test models/account_spec.rb: task #104 describe association has many to many when there is inventory feature as default.

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,16 +1,50 @@
+# == Schema Information
+#
+# Table name: accounts
+#
+#  id           :bigint           not null, primary key
+#  company_name :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  user_id      :bigint           not null
+#
+# Indexes
+#
+#  index_accounts_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 require 'rails_helper'
 
 RSpec.describe Account, type: :model do
-  describe '#create' do
-    subject(:create_valid) { FactoryBot.create(:account, user: user) }
+  describe 'associations' do
+    it { is_expected.to have_many(:account_features) }
+    it { is_expected.to have_many(:features).through(:account_features) }
+  end
 
-    let!(:user) { FactoryBot.create(:user) }
+  describe '#create' do
+    let(:user) { FactoryBot.create(:user) }
+    let(:account) { user.account }
 
     context 'when valid' do
       it 'creates account' do
         expect do
-          create_valid
+          user
         end.to change(described_class, :count).by(1)
+      end
+    end
+
+    context 'when there is inventory feature' do
+      let!(:inventory_feature) { FactoryBot.create(:feature, name: 'Inventory', feature_key: 0, is_enabled: true) }
+
+      it 'creates association account_feature' do
+        expect(account.features).to include(inventory_feature)
+      end
+
+      it 'has inventory feature enabled' do
+        expect(account.features.first).to be_is_enabled
       end
     end
   end
