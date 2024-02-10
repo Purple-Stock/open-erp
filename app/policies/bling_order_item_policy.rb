8e7522bb66f9d9711c238b1fcd974551fe5853fa
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2024-02-10 15:47:16 -0300

    feat bling_order_item_policy.rb: task #104 add authorize #index?

---
+++ app/policies/bling_order_item_policy.rb
@@ -0,0 +1,13 @@
+class BlingOrderItemPolicy < ApplicationPolicy
+  def index?
+    @account = user.account
+    @enabled ||= @account.account_features.includes([:feature])
+                        .select { |account_feature| account_feature.feature.bling_integration? }&.first&.is_enabled?
+  end
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    # def resolve
+    #   scope.all
+    # end
+  end
+end
