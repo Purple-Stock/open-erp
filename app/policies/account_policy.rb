37e6954dfe3f3a6b180552971dbb7f395bccbd15
Paulo Felipe Silva de Souza <alparakleto@gmail.com>
2024-02-10 15:53:46 -0300

    refactor account_policy.rb: task #104 memoized version.

--- app/policies/account_policy.rb
+++ app/policies/account_policy.rb
@@ -1,14 +1,11 @@
 # frozen_string_literal: true
 
 class AccountPolicy
-  attr_reader :account
+  attr_reader :account, :enabled
 
   def initialize(account)
     @account = account
-  end
-
-  def enabled?
-    bling_feature = account.account_features.includes([:account_features]).select { |account_feature| account_feature.feature.bling_integration? }
-    bling_feature&.first&.is_enabled?
+    @enabled ||= account.account_features.includes([:feature])
+                        .select { |account_feature| account_feature.feature.bling_integration? }&.first&.is_enabled?
   end
 end
