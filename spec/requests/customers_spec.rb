c092e7ac73dc510d9521baf7e59b34b69ef10906
Pauloparakleto <user.email>
2024-02-27 19:06:06 -0300

    test customers_spec.rb: task #191 authorize create action

--- spec/requests/customers_spec.rb
+++ spec/requests/customers_spec.rb
@@ -3,6 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe 'Customers', type: :request do
+  let(:valid_params) { FactoryBot.attributes_for(:customer) }
+
   describe 'GET /customers' do
     include_context 'with user signed in'
     context 'when feature bling' do
@@ -38,6 +40,27 @@ RSpec.describe 'Customers', type: :request do
     context 'without feature bling as a stock manager only' do
       it 'redirects to products list' do
         get new_customer_path
+
+        expect(response).to redirect_to(products_path)
+      end
+    end
+  end
+
+  describe 'POST /customers/' do
+    include_context 'with user signed in'
+    context 'when feature bling' do
+      include_context 'with bling feature' do
+        it 'has successfully response' do
+          post customers_path, params: { customer: valid_params }
+
+          expect(response).to redirect_to(customer_path(Customer.last))
+        end
+      end
+    end
+
+    context 'without feature bling as a stock manager only' do
+      it 'redirects to products list' do
+        post customers_path, params: { customer: valid_params }
 
         expect(response).to redirect_to(products_path)
       end
