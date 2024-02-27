b2f3ce95e367b6b203918eec8d068069ae9b222a
Pauloparakleto <user.email>
2024-02-27 11:13:41 -0300

    test purchase_products_spec.rb: task #186 describe POST purchase_products_path for valid and invalid quantity.

--- spec/requests/purchase_products_spec.rb
+++ spec/requests/purchase_products_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe 'PurchaseProduct' do
-  describe 'POST /purchase_products/save_inventory' do
+  describe 'POST purchase_products_path' do
     include_context 'with user signed in'
 
     let!(:product) { FactoryBot.create(:product, account_id: user.account.id) }
@@ -11,25 +11,33 @@ RSpec.describe 'PurchaseProduct' do
     context 'when valid' do
       it 'creates purchase product' do
         expect do
-          post save_inventory_path, params: { product_id: product.id, quantity: 1 }
+          post purchase_products_path, params: { purchase_product:
+                                                   { product_id: product.id, quantity: 1,
+                                                     account_id: user.account.id } }
         end.to change(PurchaseProduct, :count).by(1)
       end
 
       it 'redirects to product' do
-        post save_inventory_path, params: { product_id: product.id, quantity: 1 }
-        expect(response).to redirect_to(product_path(product))
+        post purchase_products_path, params: { purchase_product:
+                                                 { product_id: product.id, quantity: 1,
+                                                   account_id: user.account.id } }
+        expect(response).to redirect_to(purchase_product_path(PurchaseProduct.last))
       end
     end
 
     context 'when invalid quantity' do
       it 'does not create purchase product' do
         expect do
-          post save_inventory_path, params: { product_id: product.id, quantity: 0 }
+          post purchase_products_path, params: { purchase_product:
+                                                   { product_id: product.id, quantity: 0,
+                                                     account_id: user.account.id } }
         end.to change(PurchaseProduct, :count).by(0)
       end
 
       it 'is unprocessable entity' do
-        post save_inventory_path, params: { product_id: product.id, quantity: 0 }
+        post purchase_products_path, params: { purchase_product:
+                                                 { product_id: product.id, quantity: 0,
+                                                   account_id: user.account.id } }
         expect(response.status).to eq(422)
       end
     end
