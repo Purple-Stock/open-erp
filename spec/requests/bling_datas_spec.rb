bb8f814a1aa43027d6e65884ef8c1054fe8a4737
Pauloparakleto <user.email>
2024-03-04 17:01:17 -0300

    test bling_datas_spec.rb: task #208 fix test scenarios for show and index

--- spec/requests/bling_datas_spec.rb
+++ spec/requests/bling_datas_spec.rb
@@ -26,18 +26,18 @@ RSpec.describe 'BlingData', type: :request do
     include_context 'with user signed in'
     include_context 'with bling datum'
 
-    before { get bling_datum_path(bling_datum) }
-
     context 'when authorized' do
       include_context 'with bling feature'
 
       it 'has successfully response' do
+        get bling_datum_path(bling_datum)
         expect(response).to have_http_status(:success)
       end
     end
 
     context 'without bling feature' do
       it 'is a redirect response' do
+        get bling_datum_path(bling_datum)
         expect(response).to redirect_to(products_path)
       end
     end
