1bfe404103899634b41dabab3752d1346a34bcc2
puppe1990 <matheus.puppe90@hotmail.com>
2024-09-05 08:01:17 -0300

    add new field to item and rules

--- app/controllers/orders_control_controller.rb
+++ app/controllers/orders_control_controller.rb
@@ -20,10 +20,10 @@ class OrdersControlController < ApplicationController
                        .order(custom_id: :desc)
   end
   
-
   def show_pending_orders
     situation_id = params[:situation_id]
     store_id = params[:store_id]
+    resolution_status = params[:resolution_status]
 
     if situation_id.present?
       cleaned_situation_ids = situation_id.split(',').map(&:to_i)
@@ -31,13 +31,18 @@ class OrdersControlController < ApplicationController
       cleaned_situation_ids = BlingOrderItem::Status::PENDING
     end
 
-    pending_items = Item.includes(:bling_order_item).where(bling_order_items: { situation_id: cleaned_situation_ids })
+    items = Item.includes(:bling_order_item).where(bling_order_items: { situation_id: cleaned_situation_ids })
 
-    if store_id.present?
-      @all_items = pending_items.where(bling_order_items: { store_id: store_id })
-    else
-      @all_items = pending_items
+    items = items.where(bling_order_items: { store_id: store_id }) if store_id.present?
+    
+    case resolution_status
+    when 'unresolved'
+      items = items.unresolved
+    when 'resolved'
+      items = items.resolved
     end
+
+    @all_items = items
 
     # Group items by store and sort by total quantity
     @sorted_stores = @all_items.group_by { |item| item.bling_order_item.store_name }
@@ -46,11 +51,10 @@ class OrdersControlController < ApplicationController
 
     respond_to do |format|
       format.html # show.html.erb
-      format.csv { send_data generate_csv(@all_items), filename: "pending-orders-#{Date.today}.csv" }
+      format.csv { send_data generate_csv(@all_items), filename: "orders-#{Date.today}.csv" }
     end
   end
   
-
   def show_orders_business_day
     @simplo_orders = SimploOrder.where(order_status: %w[2 30 31]).order(order_id: :asc)
     @calendar = SimploOrder.calendar
