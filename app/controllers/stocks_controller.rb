fbeee2ff41a0450c4dd5589edaccf7b245b04788
puppe1990 <matheus.puppe90@hotmail.com>
2024-09-11 10:34:16 -0300

    update the stocks view

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -21,34 +21,42 @@ class StocksController < ApplicationController
     @default_status_filter = params['status']
     @default_situation_balance_filter = params['balance_situation']
   
-    # Fetch stocks with necessary associations
     stocks = Stock.where(account_id: current_tenant)
-                  .includes(:product)
+                  .includes(:product, :balances)
                   .only_positive_price(true)
                   .filter_by_status(params['status'])
                   .filter_by_total_balance_situation(params['balance_situation'])
   
-    # Fetch all relevant items in a single query
+    @warehouses = Warehouse.where(account_id: current_tenant).pluck(:bling_id, :description).to_h
+  
     start_date = 1.month.ago.to_date
     end_date = Date.today
   
     items_sold = Item.joins(:bling_order_item)
                      .where(account_id: current_tenant,
-                            bling_order_item: { date: start_date..end_date })
+                            bling_order_items: { date: start_date..end_date })
                      .group(:sku)
                      .sum(:quantity)
   
-    # Calculate forecasts for each stock
     stocks_with_forecasts = stocks.map do |stock|
-      sold_quantity = items_sold[stock.sku] || 0
-      forecast = [sold_quantity - stock.total_balance, 0].max
-      [stock, forecast]
+      total_sold = items_sold[stock.product.sku] || 0
+      total_physical_balance = stock.balances.sum(&:physical_balance)
+      total_forecast = [total_sold - total_physical_balance, 0].max
+
+      warehouse_forecasts = stock.balances.map do |balance|
+        warehouse_forecast = if balance.physical_balance > 0
+                               [total_sold - balance.physical_balance, 0].max
+                             else
+                               total_sold
+                             end
+        [balance.deposit_id, { sold: total_sold, forecast: warehouse_forecast }]
+      end.to_h
+      
+      [stock, { warehouses: warehouse_forecasts, total_sold: total_sold, total_forecast: total_forecast }]
     end
   
-    # Sort stocks based on forecasts (in descending order)
-    sorted_stocks = stocks_with_forecasts.sort_by { |_, forecast| -forecast }.map(&:first)
+    sorted_stocks = stocks_with_forecasts.sort_by { |_, data| -data[:total_sold] }
   
-    # Paginate the sorted stocks
-    @pagy, @stocks = pagy_array(sorted_stocks)
+    @pagy, @stocks_with_data = pagy_array(sorted_stocks)
   end
 end
