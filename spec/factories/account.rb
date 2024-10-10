612350584c214a004fdda12b7db75ad1fcc85121
puppe1990 <matheus.puppe90@hotmail.com>
2024-10-10 15:38:55 -0300

    add trait to account

--- spec/factories/account.rb
+++ spec/factories/account.rb
@@ -3,7 +3,14 @@
 FactoryBot.define do
   factory :account do
     company_name { Faker::Company.name }
-
     user_id { create(:user).id }
+
+    trait :for_production_tests do
+      # Add any specific attributes or associations needed for production tests
+      # For example:
+      # has_many :productions
+      # has_many :tailors
+      # etc.
+    end
   end
 end
