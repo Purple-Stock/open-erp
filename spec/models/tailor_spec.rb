d4728b0263e6fff29d21e4ca4eef2dc3115ab5b0
puppe1990 <matheus.puppe90@hotmail.com>
2024-10-10 15:22:14 -0300

    create spec to tailor

--- spec/models/tailor_spec.rb
+++ spec/models/tailor_spec.rb
@@ -15,5 +15,33 @@
 require 'rails_helper'
 
 RSpec.describe Tailor, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it 'is valid with valid attributes' do
+      tailor = build(:tailor)
+      expect(tailor).to be_valid
+    end
+
+    it 'is not valid without a name' do
+      tailor = build(:tailor, name: nil)
+      expect(tailor).to_not be_valid
+    end
+
+    it 'is not valid without an account' do
+      tailor = build(:tailor, account: nil)
+      expect(tailor).to_not be_valid
+    end
+  end
+
+  describe 'associations' do
+    it 'belongs to an account' do
+      association = described_class.reflect_on_association(:account)
+      expect(association.macro).to eq :belongs_to
+    end
+  end
+
+  describe 'factory' do
+    it 'has a valid factory' do
+      expect(build(:tailor)).to be_valid
+    end
+  end
 end
