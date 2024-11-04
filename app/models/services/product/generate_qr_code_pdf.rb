14872f9663518ceb826a1124e1c52e7254582204
puppe1990 <matheus.puppe90@hotmail.com>
2024-11-04 15:08:50 -0300

    improve the design of pdf of qr codes

--- app/models/services/product/generate_qr_code_pdf.rb
+++ app/models/services/product/generate_qr_code_pdf.rb
@@ -6,15 +6,32 @@ module Services
     class GenerateQrCodePdf
       include Prawn::View
 
+      QR_CODE_WIDTH = 200
+      QR_CODE_HEIGHT = 250  # Total height including QR code and text
+      HORIZONTAL_SPACING = 50
+      VERTICAL_SPACING = 30
+
       def initialize(products, copies)
         @products = products
         @copies = copies
+        
+        # Calculate page margins to center content
+        page_width = 595.28  # A4 width in points
+        page_height = 841.89 # A4 height in points
+        content_width = (QR_CODE_WIDTH * 2) + HORIZONTAL_SPACING
+        content_height = (QR_CODE_HEIGHT * 2) + VERTICAL_SPACING
+        
+        left_margin = (page_width - content_width) / 2
+        top_margin = (page_height - content_height) / 2
+
         @document = Prawn::Document.new(
           page_size: 'A4',
-          margin: [20, 20, 20, 20]
+          margin: [top_margin, left_margin, top_margin, left_margin]
         )
-        @current_x = 50
+
+        @current_x = 0
         @current_y = cursor
+        @qr_codes_on_page = 0
         generate
       end
 
@@ -23,6 +40,15 @@ module Services
       def generate
         @products.each do |product|
           @copies.times do
+            # Start new page if we've already placed 4 QR codes
+            if @qr_codes_on_page == 4
+              break unless more_qr_codes?(product)
+              start_new_page
+              @current_x = 0
+              @current_y = cursor
+              @qr_codes_on_page = 0
+            end
+
             # Get QR code image data
             qr_code_service = Services::Product::GenerateQrCode.new(product: product)
             qr_code_data = qr_code_service.call(for_download: true)
@@ -34,19 +60,19 @@ module Services
             temp_file.rewind
 
             # Add QR code image at current position
-            image temp_file.path, at: [@current_x, @current_y], width: 200
+            image temp_file.path, at: [@current_x, @current_y], width: QR_CODE_WIDTH
 
             # Add SKU and name below QR code
             text_box product.sku,
                     at: [@current_x, @current_y - 210],
-                    width: 200,
+                    width: QR_CODE_WIDTH,
                     align: :center,
                     size: 12,
                     style: :bold
 
             text_box product.name,
                     at: [@current_x, @current_y - 230],
-                    width: 200,
+                    width: QR_CODE_WIDTH,
                     align: :center,
                     size: 10
 
@@ -54,21 +80,33 @@ module Services
             temp_file.close
             temp_file.unlink
 
-            # Update position for next QR code
-            if @current_x >= 300
-              @current_x = 50
-              @current_y -= 280
+            # Update position and counter
+            @qr_codes_on_page += 1
+            if @qr_codes_on_page % 2 == 0
+              @current_x = 0
+              @current_y -= QR_CODE_HEIGHT + VERTICAL_SPACING
             else
-              @current_x += 250
-            end
-
-            # Start new page if needed
-            if @current_y < 250
-              start_new_page
-              @current_x = 50
-              @current_y = cursor
+              @current_x += QR_CODE_WIDTH + HORIZONTAL_SPACING
             end
           end
+        end
+      end
+
+      def more_qr_codes?(current_product)
+        # Check if there are more QR codes to generate after the current position
+        current_index = @products.index(current_product)
+        remaining_copies = @copies
+        
+        return false unless current_index
+
+        if current_index < @products.length - 1
+          # There are more products after this one
+          return true
+        elsif current_index == @products.length - 1
+          # This is the last product, check if there are more copies to make
+          remaining_copies > 1
+        else
+          false
         end
       end
     end
